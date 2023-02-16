5f6fce9ed37ce50009fccd14162e8dde217305e7
Kworz <romain.sapet@metalizz.com>
2023-02-16 17:07:30 +0100

    feat: FormInput background color

--- src/lib/components/FormInput.svelte
+++ src/lib/components/FormInput.svelte
@@ -14,11 +14,11 @@
     export let min: number | null | undefined = undefined;
     export let max: number | null | undefined = undefined;
 
-    export let inputClass: string | undefined = undefined;
+    export let backgroundColor = "bg-zinc-100";
 
 </script>
 
-<Flex gap={1} direction="col" class={$$props.class}>
+<Flex gap={1} direction="col">
     {#if label}
         <span class="text-zinc-700 text-sm leading-4">
             {label}
@@ -28,5 +28,5 @@
         </span>
     {/if}
     
-    <input {type} {name} {value} {min} {max} class="border border-zinc-500/50 p-2 rounded-sm bg-zinc-100 duration-200 {inputClass ?? ""}" class:ring-red-500={invalid} on:change/>
+    <input {type} {name} {value} {min} {max} class="border border-zinc-500/50 p-2 rounded-sm duration-200 {backgroundColor}" class:ring-red-500={invalid} on:change/>
 </Flex>
