f118a52ced8c37f7d43ec8b57c24cce359a42335
Kworz <romain.sapet@metalizz.com>
2023-02-17 13:25:59 +0100

    feat: redirect afeter article creation

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,7 +4,6 @@ import PocketBase from 'pocketbase';
 export const handle = (async ({ event, resolve }) => {
 
     event.locals.pb = new PocketBase("http://127.0.0.1:8090/");
-
     event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
 
     if(event.locals.pb.authStore.isValid)
--- src/routes/app/articles/new/+page.server.ts
+++ src/routes/app/articles/new/+page.server.ts
@@ -1,21 +1,24 @@
 import { redirect } from "@sveltejs/kit";
 import type { Actions } from "../$types";
+import { Collections, type ArticleResponse } from "$lib/DBTypes";
 
 export const actions: Actions = {
     create: async ({ request, locals }) =>
     {
         const formData = await request.formData();
 
+        let createdArticle = undefined;
+
         try
         {
-            await locals.pb.collection('article').create(formData);
-            throw redirect(303, "/articles");
+            createdArticle = await locals.pb.collection(Collections.Article).create<ArticleResponse>(formData);
         }
         catch(err)
         {
             console.log(err);
-
             return { error: "Failed to create object" };
         }
+
+        throw redirect(303, `/app/articles/${createdArticle.id}`);
     }
 };
