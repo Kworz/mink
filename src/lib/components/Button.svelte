5405e83aa4d9d531edffddac111d239bff0531f7
Kworz <romain.sapet@metalizz.com>
2023-02-20 15:02:32 +0100

    feat: buttons sizes

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
 
+    export let size: "base" | "small" | "tiny" = "base";
+
     export let textColor = "hover:text-white text-zinc-800";
     export let hoverColor = "hover:bg-violet-500";
     export let borderColor = "border-violet-500";
 
+    const sizes: Record<typeof size, string> = {
+        "base": "border-2 py-2 px-4 font-bold",
+        "small": "border-2 py-1 px-2 font-semibold text-sm",
+        "tiny": "border py-1 px-2 font-medium text-xs"
+    }
+
 </script>
 
-<button class="border-2 {borderColor} {hoverColor} {textColor} duration-100 py-2 px-4 rounded-sm font-bold {$$props.class}" on:click>
+<button class="{sizes[size]} {borderColor} {hoverColor} {textColor} duration-100 rounded-sm {$$props.class}" on:click>
     <slot />
 </button>
