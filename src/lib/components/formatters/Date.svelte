19174e3bc83d1b6e65399e6aaeaeea3cc76ac5d6
Kworz <romain.sapet@metalizz.com>
2023-02-28 17:54:50 +0100

    feta: created Date formatter

---
+++ src/lib/components/formatters/Date.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { Temporal } from "@js-temporal/polyfill";
+    export let date: string | undefined;
+
+</script>
+
+{#if date !== undefined && date !== ""}
+    {Temporal.Instant.from(date).toZonedDateTimeISO('UTC').toPlainDate().toString()}
+{:else}
+    {date}
+{/if}
