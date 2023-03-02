aea78acf3942ae898c3597073080601577c2b25c
Kworz <romain.sapet@metalizz.com>
2023-03-02 18:34:25 +0100

    feat: added supplier link

--- src/lib/components/supplier/Supplier.svelte
+++ src/lib/components/supplier/Supplier.svelte
@@ -8,10 +8,10 @@
 </script>
 
 {#if browser}
-    <div class="flex flex-row gap-2 items-center bg-white {margin} py-1 rounded-full ring-1 ring-zinc-300">
+    <a href={supplier.website ?? "#"} class="flex flex-row gap-2 items-center bg-white hover:bg-white/75 duration-100 {margin} py-1 rounded-full ring-1 ring-zinc-300">
         {#if supplier.thumbnail}
             <img src="http://{window.location.hostname}:8090/api/files/{supplier.collectionName}/{supplier.id}/{supplier.thumbnail}?thumb=200x200" alt={supplier.name + " Logo"} class="h-6 w-6 rounded-full" />
         {/if}
-        <div>{supplier.name}</div>
-    </div>
+        <span class="text-sm font-medium">{supplier.name}</span>
+    </a>
 {/if}
