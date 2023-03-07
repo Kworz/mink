e77347f6ad391dedda297af5770a3283b7fb513c
Kworz <romain.sapet@metalizz.com>
2023-03-07 11:07:35 +0100

    fix: suppliers with no image is drawn correctly

--- src/lib/components/supplier/Supplier.svelte
+++ src/lib/components/supplier/Supplier.svelte
@@ -2,14 +2,14 @@
     import { browser } from "$app/environment";
     import type { SuppliersResponse } from "$lib/DBTypes";
 
-    $: margin = supplier.thumbnail !== undefined  ? "pl-1 pr-3" : "px-3";
+    $: margin = supplier.thumbnail !== ""  ? "pl-1 pr-3" : "px-3";
 
     export let supplier: SuppliersResponse;
 </script>
 
 {#if browser}
-    <a href={supplier.website ?? "#"} class="flex flex-row gap-2 items-center bg-white hover:bg-white/75 duration-100 {margin} py-1 rounded-full ring-1 ring-zinc-300">
-        {#if supplier.thumbnail}
+    <a href={supplier.website ?? "#"} class="flex flex-row gap-2 items-center bg-white hover:bg-white/75 duration-100 {margin} py-1 rounded-full ring-1 ring-zinc-300 w-fit">
+        {#if supplier.thumbnail !== ""}
             <img src="http://{window.location.hostname}:8090/api/files/{supplier.collectionName}/{supplier.id}/{supplier.thumbnail}?thumb=200x200" alt={supplier.name + " Logo"} class="h-6 w-6 rounded-full" />
         {/if}
         <span class="text-sm font-medium">{supplier.name}</span>
