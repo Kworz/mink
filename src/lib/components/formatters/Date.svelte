a2765d923a78e6612d550983bee07bef3042c72a
Kworz <romain.sapet@metalizz.com>
2023-03-07 11:07:49 +0100

    feat: date colorization

--- src/lib/components/formatters/Date.svelte
+++ src/lib/components/formatters/Date.svelte
@@ -1,17 +1,31 @@
 <script lang="ts">
     import { Temporal } from "@js-temporal/polyfill";
+
     export let date: string | undefined;
 
     export let format: "long" | "medium" | "short" = "short";
+    export let colorDate = false;
 
 </script>
 
 {#if date !== undefined && date !== ""}
-    {#if format === "short"}
-        {Temporal.Instant.from(date).toZonedDateTimeISO('UTC').toPlainDate().toString()}
-    {:else}
-        {Intl.DateTimeFormat("fr", { dateStyle: format}).format(Date.parse(date))}
-    {/if}
+    {@const datePosition = Temporal.PlainDate.compare(Temporal.Instant.from(date).toZonedDateTimeISO('UTC').toPlainDate(), Temporal.Now.zonedDateTimeISO('UTC').toPlainDate())}
+    {@const isToday = datePosition === 0}
+    {@const isPassed = datePosition === -1}
+    {@const isFuture = datePosition === 1}
+    
+    <span
+        class:text-red-500={colorDate && isPassed}
+        class:text-emerald-500={colorDate && isFuture}
+        class:text-amber-500={colorDate && isToday}
+        class:font-medium={colorDate && (isPassed || isToday)}
+    >
+        {#if format === "short"}
+            {Temporal.Instant.from(date).toZonedDateTimeISO('UTC').toPlainDate().toString()}
+        {:else}
+            {Intl.DateTimeFormat("fr", { dateStyle: format}).format(Date.parse(date))}
+        {/if}
+    </span>
 {:else}
-    {date}
+    Date Inconnue
 {/if}
