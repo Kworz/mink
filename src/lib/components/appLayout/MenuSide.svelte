a349cd5e44a151668c6c9ddf2ecad6822608672b
Kworz <romain.sapet@metalizz.com>
2023-03-09 13:28:19 +0100

    feat: created menu Side

---
+++ src/lib/components/appLayout/MenuSide.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+
+    import Portal from "svelte-portal";
+
+</script>
+
+<Portal target="#side_menu">
+    <div class="h-screen shrink-0 bg-white shadow-2xl duration-300 py-6 px-8">
+        <slot />
+    </div>
+</Portal>
