ce1c63c0dd7a2634fcbe1d2150b7a4e59b7b9f8f
Kworz <romain.sapet@metalizz.com>
2023-03-13 14:45:58 +0100

    feat: updated nomenclature ui

--- src/routes/app/nomenclatures/[id]/+page.svelte
+++ src/routes/app/nomenclatures/[id]/+page.svelte
@@ -7,7 +7,7 @@
     import type { ActionData, PageData, Snapshot } from "./$types";
 
     import { Icon } from "@steeze-ui/svelte-icon";
-    import { Check, ExclamationTriangle, Wrench } from "@steeze-ui/heroicons";
+    import { Check, DocumentDuplicate, ExclamationTriangle, Wrench } from "@steeze-ui/heroicons";
     import FormInput from "$lib/components/FormInput.svelte";
     import type { NomenclatureRowResponseExpanded } from "./+page.server";
     import ArticleRow from "$lib/components/article/ArticleRow.svelte";
@@ -19,6 +19,9 @@
     import TableCell from "$lib/components/table/TableCell.svelte";
     import Price from "$lib/components/formatters/Price.svelte";
     import DetailLabel from "$lib/components/DetailLabel.svelte";
+    import Wrapper from "$lib/components/Wrapper.svelte";
+    import PillMenu from "$lib/components/PillMenu/PillMenu.svelte";
+    import PillMenuButton from "$lib/components/PillMenu/PillMenuButton.svelte";
 
     export let data: PageData;
     export let form: ActionData;
@@ -58,109 +61,108 @@
 
     $: if(form?.success === true && browser) { invalidateAll(); }
 
+    let menuPillShown = false;
+
 </script>
 
 <svelte:head><title>Nomenclature — {data.nomenclature.name}</title></svelte:head>
 
-{#if editNomenclature}
-    <form action="?/editNomenclature" method="post" use:enhanceNoReset>
+<Wrapper>
 
-        <Flex direction="col" class="w-1/3">
-            <FormInput label="Nom de la nomenclature" labelMandatory={true} name="name" value={data.nomenclature.name}/>
-            <FormInput label="Description" name="description" value={data.nomenclature.description}/>
-
-            <Flex>
-                <Button borderColor="border-emerald-500" hoverColor="hover:bg-emerald-500">Valider</Button>
-                <Button borderColor="border-red-500" hoverColor="hover:bg-red-500" on:click={() => editNomenclature = false}>Annuler</Button>
-            </Flex>
-        </Flex>
-    </form>
-{:else}
-    <h2>{data.nomenclature.name}</h2>
-    <p>{data.nomenclature.description}</p>
-    {#if data.nomenclature.expand?.['nomenclature_row(parent_nomenclature)'] !== undefined}
-        <p>Cout estimé de la nomenclature:
-            <DetailLabel>
-                <Price value={data.nomenclature.expand?.['nomenclature_row(parent_nomenclature)'].reduce((p, c) => p + (c.expand?.child_article.price ?? 0) * c.quantity_required, 0)} />
-            </DetailLabel>
-        </p>
-    {/if}
-
-    <button
-        on:click={() => editNomenclature = true}
-        class="my-2 text-violet-500 hover:text-blue-500 duration-200"
-    >
-        <Icon src={Wrench} class="h-5 w-5 inline"/>
-        Editer les informations
-    </button>
-{/if}
-
-<Flex direction="col" gap={4} class="mt-6">
-    <Flex gap={4} items="end">
+    <PillMenu bind:open={menuPillShown}>
+        <PillMenuButton icon={Wrench} on:click={() => { editNomenclature = true; menuPillShown = false;}}>Modifier les informations</PillMenuButton>
         <form action="?/copyNomenclature" method="post" use:enhanceNoReset>
-            <Button hoverColor="hover:bg-amber-500" borderColor="border-amber-500">Copier</Button>
+            <PillMenuButton role="warning" icon={DocumentDuplicate}>Copier la nomenclature</PillMenuButton>
         </form>
-    </Flex>
+    </PillMenu>
 
-    <Filter bind:filter={currentFilter} availableFilters={["name", "manufacturer", "supplier", "reference", "group", "price"]} bind:filterResult={queryFilters}/>
-</Flex>
- 
-<Table>
-    <svelte:fragment slot="head">
-        <TableHead>
-            Éléments
-            {#if data.nomenclature.expand?.['nomenclature_row(parent_nomenclature)'] !== undefined}
-                ({data.nomenclature.expand['nomenclature_row(parent_nomenclature)'].filter(k => listRowFilter(k, queryFilters)).length})
-            {/if} 
-        </TableHead>
-        <TableHead>Groupe</TableHead>
-        <TableHead>Quantité nécéssaire</TableHead>
-        <TableHead>Supprimer</TableHead>
-    </svelte:fragment>
-
-    <svelte:fragment slot="body">
+    {#if editNomenclature}
+        <form action="?/editNomenclature" method="post" use:enhanceNoReset>
+    
+            <Flex direction="col" class="w-1/3">
+                <FormInput label="Nom de la nomenclature" labelMandatory={true} name="name" value={data.nomenclature.name}/>
+                <FormInput label="Description" name="description" value={data.nomenclature.description}/>
+    
+                <Flex>
+                    <Button borderColor="border-emerald-500" hoverColor="hover:bg-emerald-500">Valider</Button>
+                    <Button borderColor="border-red-500" hoverColor="hover:bg-red-500" on:click={() => editNomenclature = false}>Annuler</Button>
+                </Flex>
+            </Flex>
+        </form>
+    {:else}
+        <h2>{data.nomenclature.name}</h2>
+        <p>{data.nomenclature.description}</p>
         {#if data.nomenclature.expand?.['nomenclature_row(parent_nomenclature)'] !== undefined}
-            {#each data.nomenclature.expand['nomenclature_row(parent_nomenclature)'].filter(k => listRowFilter(k, queryFilters)) as row}
-                <TableRow>
-                    <TableCell>
-                        {#if row.expand?.child_article !== undefined}
-                            <ArticleRow article={row.expand?.child_article} />
-                        {:else}
-                            <span class="text-red-500">Failed to load Article data.</span>
-                        {/if}
-                    </TableCell>
-                    <TableCell>
-                        <form action="?/editItem" method="post" use:enhanceNoReset>
-                            <input type="hidden" id="row_id" value={row.id} name="row_id"/>
-                            <Flex items="center">
-                                <FormInput type="text" name="group" bind:value={row.group} backgroundColor="bg-white" />
-                                <Button size="small"><Icon src={Check} class="h-4 w-4"/></Button>
-                            </Flex>
-                            
-                        </form>
-                    </TableCell>
-                    <TableCell>
-                        <form action="?/editItem" method="post" use:enhanceNoReset>
-                            <input type="hidden" id="row_id" value={row.id} name="row_id"/>
-                            <Flex items="center">
-                                <FormInput type="number" name="quantity_required" bind:value={row.quantity_required} backgroundColor="bg-white" />
-                                <Button size="small"><Icon src={Check} class="h-4 w-4"/></Button>
-                            </Flex>
-                        </form>
-                    </TableCell>
-                    <TableCell>
-                        <form action="?/deleteItem" method="post" use:enhanceNoReset>
-                            <input type="hidden" id="row_id" value={row.id} name="row_id"/>
-                            <Button borderColor="border-red-500" hoverColor="hover:bg-red-500" size="small">Supprimer</Button>
-                        </form>
-                    </TableCell>
-                </TableRow>  
-            {/each}
-        {:else}
-            <p class="p-6 font-medium text-lg">
-                <Icon src={ExclamationTriangle} theme="solid" class="h-10 w-10 mr-2 text-orange-500 inline"/>
-                Aucun élément présent dans la nomenclature.
+            <p>Cout estimé de la nomenclature:
+                <DetailLabel>
+                    <Price value={data.nomenclature.expand?.['nomenclature_row(parent_nomenclature)'].reduce((p, c) => p + (c.expand?.child_article.price ?? 0) * c.quantity_required, 0)} />
+                </DetailLabel>
             </p>
         {/if}
-    </svelte:fragment>
-</Table>
+    {/if}
+</Wrapper>
+
+<Wrapper class="mt-6">
+    <Filter bind:filter={currentFilter} availableFilters={["name", "manufacturer", "supplier", "reference", "group", "price"]} bind:filterResult={queryFilters} />
+
+    <Table embeded={true}>
+        <svelte:fragment slot="head">
+            <TableHead>
+                Éléments
+                {#if data.nomenclature.expand?.['nomenclature_row(parent_nomenclature)'] !== undefined}
+                    ({data.nomenclature.expand['nomenclature_row(parent_nomenclature)'].filter(k => listRowFilter(k, queryFilters)).length})
+                {/if} 
+            </TableHead>
+            <TableHead>Groupe</TableHead>
+            <TableHead>Quantité nécéssaire</TableHead>
+            <TableHead>Supprimer</TableHead>
+        </svelte:fragment>
+    
+        <svelte:fragment slot="body">
+            {#if data.nomenclature.expand?.['nomenclature_row(parent_nomenclature)'] !== undefined}
+                {#each data.nomenclature.expand['nomenclature_row(parent_nomenclature)'].filter(k => listRowFilter(k, queryFilters)) as row}
+                    <TableRow>
+                        <TableCell>
+                            {#if row.expand?.child_article !== undefined}
+                                <ArticleRow article={row.expand?.child_article} />
+                            {:else}
+                                <span class="text-red-500">Failed to load Article data.</span>
+                            {/if}
+                        </TableCell>
+                        <TableCell>
+                            <form action="?/editItem" method="post" use:enhanceNoReset>
+                                <input type="hidden" id="row_id" value={row.id} name="row_id"/>
+                                <Flex items="center">
+                                    <FormInput type="text" name="group" bind:value={row.group} backgroundColor="bg-white" />
+                                    <Button size="small"><Icon src={Check} class="h-4 w-4"/></Button>
+                                </Flex>
+                                
+                            </form>
+                        </TableCell>
+                        <TableCell>
+                            <form action="?/editItem" method="post" use:enhanceNoReset>
+                                <input type="hidden" id="row_id" value={row.id} name="row_id"/>
+                                <Flex items="center">
+                                    <FormInput type="number" name="quantity_required" bind:value={row.quantity_required} backgroundColor="bg-white" />
+                                    <Button size="small"><Icon src={Check} class="h-4 w-4"/></Button>
+                                </Flex>
+                            </form>
+                        </TableCell>
+                        <TableCell>
+                            <form action="?/deleteItem" method="post" use:enhanceNoReset>
+                                <input type="hidden" id="row_id" value={row.id} name="row_id"/>
+                                <Button borderColor="border-red-500" hoverColor="hover:bg-red-500" size="small">Supprimer</Button>
+                            </form>
+                        </TableCell>
+                    </TableRow>  
+                {/each}
+            {:else}
+                <p class="p-6 font-medium text-lg">
+                    <Icon src={ExclamationTriangle} theme="solid" class="h-10 w-10 mr-2 text-orange-500 inline"/>
+                    Aucun élément présent dans la nomenclature.
+                </p>
+            {/if}
+        </svelte:fragment>
+    </Table>
+</Wrapper>
+ 
