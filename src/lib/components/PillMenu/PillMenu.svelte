cfa8870b289faa71bcc8bc20e6d8fc61b2c20228
Kworz <romain.sapet@metalizz.com>
2023-03-13 11:26:59 +0100

    feat: Pill menu component

--- src/lib/components/PillMenu/PillMenu.svelte
+++ src/lib/components/PillMenu/PillMenu.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
 
     import { EllipsisHorizontal } from "@steeze-ui/heroicons";
-    import { Icon } from "@steeze-ui/svelte-icon";
-    import { scale } from "svelte/transition";
+    import { scale, fade } from "svelte/transition";
     import RoundButton from "../RoundButton.svelte";
-    let open = false;
+
+    export let open = false;
 
 </script>
 
-<div class="relative">
-    <RoundButton icon={EllipsisHorizontal} on:click={() => open = !open} />
+<div class="absolute top-4 right-4">
+    <RoundButton icon={EllipsisHorizontal} on:click={() => open = !open} class={open ? "z-50" : ""}/>
     {#if open}
-        <div class="bg-gray-200 rounded-md top-[110%] right-0 absolute p-2 flex flex-col gap-2" in:scale out:scale>
+        <div class="fixed inset-0 backdrop-blur-lg z-40" in:fade={{ duration: 100 }} out:fade={{ duration:100 }}/>
+        <div class="bg-gray-200 rounded-md top-[calc(100%+1rem)] right-0 absolute p-4 flex flex-col items-start gap-3 w-max" in:scale={{ duration: 100 }} class:z-50={open} out:scale={{ duration:100 }}>
             <slot />
         </div>
     {/if}
---
+++ src/lib/components/PillMenu/PillMenuButton.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+
+    import type { IconSource } from "@steeze-ui/heroicons/types";
+    import { Icon } from "@steeze-ui/svelte-icon";
+
+    export let icon: IconSource;
+    export let role: "primary" | "secondary" | "danger" | "warning" = "primary";
+
+    export let href: string | undefined = undefined;
+
+    const roles: Record<typeof role, string> = {
+        "primary": "hover:text-violet-500",
+        "secondary": "hover:text-blue-500",
+        "danger": "hover:text-red-500",
+        "warning": "hover:text-amber-500"
+    };
+
+</script>
+
+{#if href === undefined}
+    <button on:click class="{roles[role]} duration-100">
+        <Icon src={icon} class="h-4 w-4 inline mr-2"/>
+        <slot />
+    </button>
+{:else}
+    <a {href} class="{roles[role]} duration-100">
+        <Icon src={icon} class="h-4 w-4 inline mr-2" />
+        <slot />
+    </a>
+{/if}
