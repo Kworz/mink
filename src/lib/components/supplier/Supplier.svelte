e0bf46565b24197fea44deccf45edfa404cb9422
Kworz <romain.sapet@metalizz.com>
2023-03-13 15:10:09 +0100

    chore: update styles

--- src/lib/components/supplier/Supplier.svelte
+++ src/lib/components/supplier/Supplier.svelte
@@ -8,7 +8,7 @@
 </script>
 
 {#if browser}
-    <a href={supplier.website ?? "#"} class="flex flex-row gap-2 items-center bg-white hover:bg-white/75 duration-100 {margin} py-1 rounded-full ring-1 ring-zinc-300 w-fit">
+    <a href={supplier.website ?? "#"} class="flex flex-row gap-2 items-center bg-white hover:bg-white/90 dark:bg-zinc-700 dark:hover:bg-zinc-700/80 duration-100 {margin} py-1 rounded-full border dark:border-zinc-800/50 w-fit">
         {#if supplier.thumbnail !== ""}
             <img src="http://{window.location.hostname}:8090/api/files/{supplier.collectionName}/{supplier.id}/{supplier.thumbnail}?thumb=200x200" alt={supplier.name + " Logo"} class="h-6 w-6 rounded-full" />
         {/if}
