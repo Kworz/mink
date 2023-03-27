6367078c405cadd637562df49d37f29a02a2a11f
Kworz <romain.sapet@metalizz.com>
2023-03-27 18:13:47 +0200

    fix: inline flex

--- src/lib/components/user/User.svelte
+++ src/lib/components/user/User.svelte
@@ -7,7 +7,7 @@
 
 </script>
 
-<a href="/app/users/{user.id}" class="flex flex-row items-center gap-3 p-1 pr-4 rounded-full bg-zinc-700 dark:bg-white text-white dark:text-zinc-700 hover:text-violet-400">
+<a href="/app/users/{user.id}" class="inline-flex flex-row items-center gap-3 p-1 pr-4 rounded-full bg-zinc-700 dark:bg-white text-white dark:text-zinc-700 hover:text-violet-400">
     {#if user.avatar !== "" && user.avatar !== undefined && browser}
         <img src="http://{window.location.hostname}:8090/api/files/{user.collectionName}/{user.id}/{user.avatar}?thumb=100x100" class="w-6 h-6 rounded-full" alt={user.username + " avatar"} />
     {:else}
