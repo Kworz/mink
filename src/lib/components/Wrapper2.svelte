9a9914ba084f165cf10acddd3ed1703bbb954907
Kworz <romain.sapet@metalizz.com>
2023-04-24 15:48:52 +0200

    feat: wrapped has padding export

--- src/lib/components/Wrapper2.svelte
+++ src/lib/components/Wrapper2.svelte
@@ -1,1 +1,5 @@
-<div class="p-6 rounded-xl drop-shadow-sm bg-gray-200 dark:bg-zinc-700 {$$props.class || ''}"><slot /></div>
+<script lang="ts">
+    export let padding = "p-6"
+</script>
+
+<div class="{padding} rounded-xl drop-shadow-sm bg-gray-200 dark:bg-zinc-700 {$$props.class || ''}"><slot /></div>
