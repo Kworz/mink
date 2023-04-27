0ee7275e73a850e21b442cf3644fb556dd49d132
Kworz <romain.sapet@metalizz.com>
2023-04-27 17:01:50 +0200

    fix: updated supplier edit

--- src/lib/components/appLayout/MenuSide.svelte
+++ src/lib/components/appLayout/MenuSide.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
 
-    import { Bars3, XMark } from "@steeze-ui/heroicons";
+    import { XMark } from "@steeze-ui/heroicons";
     import { Icon } from "@steeze-ui/svelte-icon";
     import Portal from "svelte-portal";
-    import { menuRightShrinked } from "./menuContext";
 
     import { createEventDispatcher } from "svelte";
 
@@ -14,10 +13,10 @@
 </script>
 
 <Portal target="#side_menu">
-    <div class="h-screen shrink-0 bg-white dark:bg-zinc-800 shadow-2xl duration-300 {$menuRightShrinked ? "p-4" : "py-6 px-8"} w-max">
+    <div class="h-screen shrink-0 bg-white dark:bg-zinc-800 shadow-2xl duration-300 py-6 px-8 w-max">
 
         {#if closable}
-            <button on:click={() => { dispatch('close'); $menuRightShrinked = !$menuRightShrinked; }} class="py-1.5 px-3 bg-gray-100 hover:bg-gray-200 duration-200 rounded-md">
+            <button on:click={() => { dispatch('close'); }} class="py-1.5 px-3 bg-gray-100 hover:bg-gray-200 duration-200 rounded-md">
                 <span class="text-red-500">
                     <Icon src={XMark} class="h-4 w-4 inline-block" />
                     Fermer le menu
@@ -25,7 +24,7 @@
             </button>
         {/if}
         
-        <div class="mt-8 {!$menuRightShrinked ? "w-max" : "w-0 overflow-clip"} duration-300">
+        <div class="mt-8 w-max duration-300">
             <slot />
         </div>
     </div>
--- src/routes/app/suppliers/+page.svelte
+++ src/routes/app/suppliers/+page.svelte
@@ -32,6 +32,7 @@
 {#if editSupplier !== undefined || createSupplier}
 
     <MenuSide closable on:close={() => { editSupplier = undefined; createSupplier = false; }}>
+
         <form action={createSupplier ? "?/createSupplier" : "?/editSupplier"} method="post" use:enhance>
 
             {#if !createSupplier}
@@ -40,7 +41,7 @@
 
             <Flex direction="col">
 
-                {#if editSupplier?.thumbnail !== "" && browser}
+                {#if (editSupplier?.thumbnail ?? "") !== "" && browser}
                     <Flex items="center" gap={2}>
                         <img src={`http://${window.location.hostname}:8090/api/files/${editSupplier?.collectionName}/${editSupplier?.id}/${editSupplier?.thumbnail}`} class="h-8 inline-block mr-4 rounded-md" alt="logo" />
                         <Button size="small" role="danger" on:click={() => { editSupplier.thumbnail = "" }}>Supprimer l'image</Button>
@@ -60,7 +61,7 @@
 
                 <FormInput type="select" name="payment_rule" value={editSupplier?.payment_rule ?? ""} label="Conditions de paiement" >
                     <option value={undefined}>—</option>
-                    <option value="order">à la commande</option>
+                    <option value="order">À la commande</option>
                     <option value="received">À la reception</option>
                     <option value="30eom">30 Jours fin du mois</option>
                     <option value="45eom">45 Jours fin du mois</option>
