c5adf4620cd3bdce5e203ad4861630d9b682016f
Kworz <romain.sapet@metalizz.com>
2023-05-31 10:09:13 +0200

    fix: filter label could send form

--- src/lib/components/filter/FilterLabel.svelte
+++ src/lib/components/filter/FilterLabel.svelte
@@ -10,7 +10,7 @@
 
 <div class="py-0.5 pr-1 pl-2 text-xs rounded-full bg-white dark:bg-zinc-800 border dark:border-zinc-700 flex flex-row items-center gap-1">
     <span>{filter.field} {filter.operator} {filter.value}</span>
-    <button on:click>
+    <button on:click|preventDefault>
         <Icon src={XMark} class="h-4 w-4 text-red-500 hover:text-red-500/75 duration-100" />
     </button>
 </div>
