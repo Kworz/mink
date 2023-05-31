3bc6d19c6d19959aa3b66104644257909534053d
Kworz <romain.sapet@metalizz.com>
2023-05-31 15:54:08 +0200

    feat: stores supports undefined values

--- src/lib/components/store/Store.svelte
+++ src/lib/components/store/Store.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
 
-    import type { ArticleStoresResponse } from "$lib/DBTypes";
-    export let store: ArticleStoresResponse;
+    import type { StoresResponse } from "$lib/DBTypes";
+    export let store: StoresResponse | undefined;
 
 </script>
 
 <span class="bg-white hover:bg-white/90 dark:bg-zinc-700 dark:hover:bg-zinc-700/80 duration-100 py-1 px-2 rounded-full border dark:border-zinc-800/50 text-xs">
-    <span class="font-semibold">{store.name}</span> / {store.location}
+    {#if store === undefined}
+        Inconnu
+    {:else}
+        <span class="font-semibold">{store.name}</span> / {store.location}
+    {/if}
 </span>
