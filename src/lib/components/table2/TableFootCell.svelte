23ec0c468135b13217a89d90d2b36418831c1372
Kworz <romain.sapet@metalizz.com>
2023-06-22 11:29:03 +0200

    feat: created table2 footCell

---
+++ src/lib/components/table2/TableFootCell.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { itemsPosition, type FlexItemPosition } from "../layout/flexTypes";
+    export let position: FlexItemPosition = "start";
+
+</script>
+
+<div class="p-4 border-t border-t-violet-500 font-semibold flex flex-col h-full justify-around transition-all duration-300 {itemsPosition[position]}">
+    <slot />
+</div>
+
