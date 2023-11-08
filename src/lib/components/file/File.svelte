fdccc5bc5973176bad75e7b1962964ea680a262a
Kworz <romain.sapet@metalizz.com>
2023-11-08 22:02:09 +0100

    updated file component, might be deprecated

--- src/lib/components/file/File.svelte
+++ src/lib/components/file/File.svelte
@@ -8,19 +8,16 @@
 
     import StlFile from "./STLFile.svelte";
 
-    import { env } from "$env/dynamic/public";
     import { Canvas } from "@threlte/core";
 
-    export let fileName: string;
+    export let fileURL: string;
     export let fancyFileName: string | undefined;
-    export let collectionName: string;
-    export let collectionID: string;
-    export let isPinned = false;
+    export let attachedArticleID: string;
 
-    $: isImage = fileName.match(/(.*\/)*.+\.(png|jpg|gif|bmp|jpeg|webp|PNG|JPG|GIF|BMP|JPEG|WEBP)/)?.at(0) !== undefined;
-    $: isStlFile = fileName.match(/(.*\/)*.+\.(stl|STL)/)?.at(0) !== undefined;
-    $: isDXFFile = fileName.match(/(.*\/)*.+\.(dxf|DXF)/)?.at(0) !== undefined;
-    $: isPDFFile = fileName.match(/(.*\/)*.+\.(pdf|PDF)/)?.at(0) !== undefined;
+    $: isImage = fileURL.match(/(.*\/)*.+\.(png|jpg|gif|bmp|jpeg|webp|PNG|JPG|GIF|BMP|JPEG|WEBP)/)?.at(0) !== undefined;
+    $: isStlFile = fileURL.match(/(.*\/)*.+\.(stl|STL)/)?.at(0) !== undefined;
+    $: isDXFFile = fileURL.match(/(.*\/)*.+\.(dxf|DXF)/)?.at(0) !== undefined;
+    $: isPDFFile = fileURL.match(/(.*\/)*.+\.(pdf|PDF)/)?.at(0) !== undefined;
 
 </script>
 
@@ -28,32 +25,26 @@
     <div class="aspect-square h-full bg-white relative">
     
         <Flex class="absolute top-4 right-4 opacity-50 hover:opacity-100 duration-200" gap={1}>
-            {#if isImage}
-                <form action="/app/scm/articles/{collectionID}?/pinAttachedFile" method="post" use:enhanceNoReset>
-                    <input type="hidden" name="pinned_file" value={isPinned ? "" : fileName} />
-                    <button><Icon src={Star} class="h-6 w-6 text-blue-400" theme={isPinned ? "solid" : ""}/></button>
-                </form>
-            {/if}
             {#if !isImage}
-                <a href="http://{env.PUBLIC_POCKETBASE_ADDRESS}/api/files/{collectionName}/{collectionID}/{fileName}" download={(fancyFileName) ? `${fancyFileName}.${fileName.split(".").at(-1)}` : fileName}>
+                <a href={fileURL} download={(fancyFileName) ? `${fancyFileName}.${fileURL.split(".").at(-1)}` : fileURL}>
                     <Icon src={ArrowDownTray} class="h-6 w-6 text-blue-400" />
                 </a>
             {/if}
-            <form action="/app/scm/articles/{collectionID}?/removeAttachedFile" method="post" use:enhanceNoReset>
-                <input type="hidden" name="attached_files-" value={fileName} />
+            <form action="/app/scm/articles/{attachedArticleID}?/removeAttachedFile" method="post" use:enhanceNoReset>
+                <input type="hidden" name="attached_files-" value={fileURL} />
                 <button><Icon src={XMark} class="h-6 w-6 text-red-400"/></button>
             </form>
         </Flex>
         {#if isImage}
-            <img src="http://{env.PUBLIC_POCKETBASE_ADDRESS}/api/files/{collectionName}/{collectionID}/{fileName}" alt={fileName} class="aspect-square object-cover h-full" />
+            <img src={fileURL} alt={fancyFileName} class="aspect-square object-cover h-full" />
         {:else if isStlFile}
             <Canvas>
-                <StlFile url="http://{env.PUBLIC_POCKETBASE_ADDRESS}/api/files/{collectionName}/{collectionID}/{fileName}" />
+                <StlFile url={fileURL} />
             </Canvas>
         {:else if isPDFFile}
-            <iframe src="http://{env.PUBLIC_POCKETBASE_ADDRESS}/api/files/{collectionName}/{collectionID}/{fileName}" height="100%" width="100%" title="PDF File"></iframe>
+            <iframe src={fileURL} height="100%" width="100%" title="PDF File"></iframe>
         {:else if isDXFFile}
-            <DxFfile url="http://{env.PUBLIC_POCKETBASE_ADDRESS}/api/files/{collectionName}/{collectionID}/{fileName}" />
+            <DxFfile url={fileURL} />
         {:else}
             <Flex class="inset-0 m-2" direction="col">
                 <Icon src={Document} class="h-32 aspect-square text-zinc-800/50" />
