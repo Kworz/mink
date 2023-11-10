3ffa4b7c4ccdf6f4f5822b0fbac470e099be24b9
Kworz <romain.sapet@metalizz.com>
2023-11-10 11:25:48 +0100

    made supplier use good logo field

--- src/lib/components/supplier/Supplier.svelte
+++ src/lib/components/supplier/Supplier.svelte
@@ -1,18 +1,16 @@
 <script lang="ts">
     import { browser } from "$app/environment";
-    import type { SuppliersResponse } from "$lib/DBTypes";
+    import type { SCMSupplier } from "@prisma/client";
 
-    import { env } from "$env/dynamic/public";
+    $: margin = supplier.logo !== null  ? "pl-1 pr-3" : "px-3";
 
-    $: margin = supplier.thumbnail !== ""  ? "pl-1 pr-3" : "px-3";
-
-    export let supplier: SuppliersResponse;
+    export let supplier: SCMSupplier;
 </script>
 
 {#if browser}
     <a href={`/app/scm/articles?filter=${encodeURIComponent('supplier ~ "' + supplier.id + '"')}`} class="flex flex-row gap-2 items-center bg-white hover:bg-white/90 dark:bg-zinc-700 dark:hover:bg-zinc-700/80 duration-100 {margin} py-1 rounded-full border dark:border-zinc-800/50 w-fit">
-        {#if supplier.thumbnail !== ""}
-            <img src="http://{env.PUBLIC_POCKETBASE_ADDRESS}/api/files/{supplier.collectionName}/{supplier.id}/{supplier.thumbnail}?thumb=200x200" alt={supplier.name + " Logo"} class="h-6 w-6 rounded-full" />
+        {#if supplier.logo !== null}
+            <img src={supplier.logo} alt={supplier.name + " Logo"} class="h-6 w-6 rounded-full" />
         {/if}
         <span class="text-sm font-medium">{supplier.name}</span>
     </a>
