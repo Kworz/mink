70087c4b7519fdf8ec47317c6f0360910d033562
Kworz <romain.sapet@metalizz.com>
2023-11-27 00:26:58 +0100

    adjusted forminput

--- src/lib/components/FormInput.svelte
+++ src/lib/components/FormInput.svelte
@@ -62,7 +62,7 @@
 
 <Flex gap={1} direction="col" class={parentClass}>
     {#if label && type !== "checkbox"}
-        <span class="text-zinc-700 dark:text-white text-sm leading-4">
+        <span class="text-white text-sm leading-4 w-max pl-0.5 pr-1">
             {label}
             {#if labelMandatory}
                 <span class="text-red-500">*</span>
