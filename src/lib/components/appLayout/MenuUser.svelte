c97b635ca75025994af119c73211b19f43a67332
Kworz <romain.sapet@metalizz.com>
2023-11-27 13:42:37 +0100

    make hover on disconnect link not reset session

--- src/lib/components/appLayout/MenuUser.svelte
+++ src/lib/components/appLayout/MenuUser.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-    import { page } from "$app/stores";
     import type { User } from "@prisma/client";
 
     import { ArrowRightOnRectangle, User as UserIcon } from "@steeze-ui/heroicons";
@@ -7,7 +6,6 @@
     import { fade } from "svelte/transition";
 
     export let user: User | null | undefined;
-
 </script>
 
 <a href="/app/users/{user?.id}">
@@ -22,7 +20,7 @@
         
         <span in:fade={{ duration: 200 }} out:fade={{ duration: 50 }} class="group-hover:inline-block hidden text-white">{user?.username}</span>
 
-        <a href="/logout" class="absolute right-0 top-0 bottom-0 items-center justify-center text-xs text-red-500 hover:text-red-600/50 group-hover:flex hidden">
+        <a href="/logout" data-sveltekit-preload-data={false} class="absolute right-0 top-0 bottom-0 items-center justify-center text-xs text-red-500 hover:text-red-600/50 group-hover:flex hidden">
             Déconnexion
             <Icon src={ArrowRightOnRectangle} class="h-4 w-4 ml-1 inline-block" />
         </a>
