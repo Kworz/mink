c6170a01a82301421da58bfcd38ae1ce54907c3a
Kworz <romain.sapet@metalizz.com>
2023-11-29 03:09:58 +0100

    Update RoundButton styling

--- src/lib/components/RoundButton.svelte
+++ src/lib/components/RoundButton.svelte
@@ -6,6 +6,6 @@
 
 </script>
 
-<button class="p-1 aspect-square bg-gray-200 dark:bg-zinc-600 dark:hover:bg-zinc-700/90 hover:bg-gray-300 text-gray-500 dark:text-gray-200 duration-100 rounded-full {$$props.class || ""}" on:click>
+<button class="p-1 aspect-square bg-zinc-600 hover:bg-zinc-700/90 dark:text-gray-200 duration-100 rounded-full {$$props.class || ""}" on:click>
     <Icon src={icon} class="h-4 w-4"/>
 </button>
