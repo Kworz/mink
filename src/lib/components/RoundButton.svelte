fa951da1f9fa3d57db23c12237d389bd47ed9780
Kworz <romain.sapet@metalizz.com>
2024-01-04 17:29:34 +0100

    updated pillMenu

--- src/lib/components/RoundButton.svelte
+++ src/lib/components/RoundButton.svelte
@@ -6,6 +6,6 @@
 
 </script>
 
-<button class="p-1 aspect-square bg-zinc-600 hover:bg-zinc-700/90 dark:text-gray-200 duration-100 rounded-full {$$props.class || ""}" on:click>
+<button class="p-1 aspect-square bg-zinc-600 hover:bg-zinc-700/90 text-white ring-1 ring-zinc-400/50 duration-100 rounded-full {$$props.class || ""}" on:click>
     <Icon src={icon} class="h-4 w-4"/>
 </button>
