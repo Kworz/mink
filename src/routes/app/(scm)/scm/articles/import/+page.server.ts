48865e60b884eb674eacf61b16bdc6d8a876cf59
Kworz <romain.sapet@metalizz.com>
2024-01-05 21:10:08 +0100

    refactored article import server action

--- src/routes/app/(scm)/scm/articles/import/+page.server.ts
+++ src/routes/app/(scm)/scm/articles/import/+page.server.ts
@@ -1,21 +1,36 @@
-import { Collections } from "$lib/DBTypes";
+import type { SCMArticle } from "@prisma/client";
 import { redirect, type Actions } from "@sveltejs/kit";
 
 export const actions: Actions = {
     import: async ({ request, locals }) => {
         try
         {
+            // TODO: Check this
             const form = await request.formData();
+            const file = form.get("file");
+            const columnOrder = form.get("columnOrder");
 
-            const file = form.get("file") as File;
+            if(!(file instanceof File))
+                throw "article.import.file.missing";
+
+            if(columnOrder === null)
+                throw "article.import.columnOrder.missing";
     
-            console.log(await file.text());
-    
-            const json: Array<{name: string, quantity: number, reference: string, manufacturer: string, price: number}> = JSON.parse(await file.text());
-    
-            for(const row of json)
+            const fileContent = await file.text();
+            const columns = JSON.parse(columnOrder.toString()) as (string | null)[];
+
+            const lines = fileContent.split("\r\n").map(line => line.split(","));
+
+            for(const line of lines)
             {
-                await locals.pb.collection(Collections.Article).create(row);
+                const article = {
+                    name: line[columns.indexOf("name")],
+                    reference: line[columns.indexOf("reference")],
+                    brand: line[columns.indexOf("manufacturer")],
+                    
+                } satisfies Partial<SCMArticle>;
+
+                await locals.prisma.sCMArticle.create({ data: article });
             }
     
             throw redirect(303, "/app/scm/articles");
