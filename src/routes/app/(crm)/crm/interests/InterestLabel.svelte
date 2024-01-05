5dae4e186c77c25e8e326d724ed509c4a3821756
Kworz <romain.sapet@metalizz.com>
2024-01-05 01:25:56 +0100

    updated crm interest typing

--- src/routes/app/(crm)/crm/interests/InterestLabel.svelte
+++ src/routes/app/(crm)/crm/interests/InterestLabel.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
     
-    import type { CrmInterestResponse } from "$lib/DBTypes";
-    export let interest: CrmInterestResponse;
+    import type { crm_interest } from "@prisma/client";
+    export let interest: crm_interest;
 
 </script>
 
 <span class="py-1 px-3 text-white font-medium {interest.color} rounded-full">{interest.name}</span>
 
-<style>
+<style lang="postcss">
     .background-rainbow {
         @apply bg-gradient-to-r from-red-500 via-yellow-500 to-green-500;
     }
