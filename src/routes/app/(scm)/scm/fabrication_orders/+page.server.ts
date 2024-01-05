28374870a28598a358db646e6741eea4ecae96de
Kworz <romain.sapet@metalizz.com>
2024-01-05 01:43:06 +0100

    updated fabrication order prisma request

--- src/routes/app/(scm)/scm/fabrication_orders/+page.server.ts
+++ src/routes/app/(scm)/scm/fabrication_orders/+page.server.ts
@@ -1,19 +1,11 @@
-import { Collections, type ArticleResponse, type FabricationOrdersResponse, type UsersResponse, type ProjectsResponse } from "$lib/DBTypes";
 import type { PageServerLoad } from "./$types";
-
-export type FabricationOrdersResponseExpanded = FabricationOrdersResponse<{
-    article: ArticleResponse,
-    applicant: UsersResponse,
-    receiver: UsersResponse,
-    project: ProjectsResponse
-}>
 
 export const load = (async ({ locals }) => {
 
-    const fabricationOrders = await locals.pb.collection(Collections.FabricationOrders).getFullList<FabricationOrdersResponseExpanded>({ expand: 'article,applicant,receiver,project'});
+    const fabricationOrders = await locals.prisma.sCMFabricationOrders.findMany();
 
     return {
-        fabricationOrders: structuredClone(fabricationOrders)
+        fabricationOrders
     }
 
 }) satisfies PageServerLoad;
