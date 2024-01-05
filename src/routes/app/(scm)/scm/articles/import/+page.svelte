a8664cf389da3602747267cfb8b3550497a5be34
Kworz <romain.sapet@metalizz.com>
2024-01-05 01:27:09 +0100

    updated article import parsing method

--- src/routes/app/(scm)/scm/articles/import/+page.svelte
+++ src/routes/app/(scm)/scm/articles/import/+page.svelte
@@ -1,79 +1,90 @@
 <script lang="ts">
-    import { env } from "$env/dynamic/public";
     import Button from "$lib/components/Button.svelte";
     import FormInput from "$lib/components/FormInput.svelte";
     import Flex from "$lib/components/layout/flex.svelte";
-    import Table from "$lib/components/table/Table.svelte";
+    import Table from "$lib/components/table2/Table.svelte";
+    import TableCell from "$lib/components/table2/TableCell.svelte";
 
     let file: File | undefined = undefined;
-    let fileGoodContent = false;
+    let columns: (string | null)[] = [];
+
+    const availableColumns = ["Référence", "Désignation", "Fabricant"];
+
+    const parseCSV = async (file: File): Promise<{ headers: string[], lines: string[][]}> => {
+
+        const text = await file.text();
+
+        const lines = text.split("\r\n");
+        const headers = lines[0].split(",");
+
+        const headersLength = headers.length;
+        const linesArray = [];
+
+        for(const line of lines.slice(1))
+        {
+            const lineArray = line.split(",");
+
+            console.log(lineArray.length, lineArray);
+
+            if(lineArray.length === headersLength)
+                linesArray.push(lineArray);
+            else if(lineArray.length > 0)
+                linesArray.push([...lineArray, ...Array(headersLength - linesArray.length).fill("") as string[]].slice(0, headersLength));
+        }
+
+        columns = Array(headersLength).fill(null);
+
+        return {
+            headers,
+            lines: linesArray
+        }        
+    }
 </script>
 
-<svelte:head><title>Article — Import</title></svelte:head>
+<svelte:head>
+    <title>Article — Import</title>
+</svelte:head>
 
 <h2>Importer des articles</h2>
-<p>Importer des articles a l'aide d'un fichier.</p>
+<p>Importer des articles a l'aide d'un fichier CSV.</p>
 
 <form action="?/import" method="post" class="mt-8" enctype='multipart/form-data'>
     <Flex direction="col" class="w-1/3">
 
-        <FormInput type="file" name="file" labelMandatory={true} on:change={(e)=> {
+        <input type="file" name="file" on:change={(e)=> {
             file = e.target.files.item(0);
-        }}/>
+        }} />
+
+        <input type="hidden" name="columnOrder" value={JSON.stringify(columns)} />
 
         {#if file !== undefined}
-            <Button class="self-start">Importer</Button>
+            <Button class="self-start mb-6">Importer</Button>
         {/if}
 
     </Flex>
 </form>
 
-{#if file !== undefined}    
-    {#await file.text()}
-        <p>Loading JSON file</p>
-    {:then textContent} 
-        {@const json = JSON.parse(textContent)}   
+{#if file !== undefined}
+    {#await parseCSV(file)}
+        <p>Parsing CSV file</p>
+    {:then parsed}
+        <Table headers={parsed.headers.map(h => ({ label: h }))}>
+            {#each parsed.headers as _, i}
+                <TableCell>
+                    <FormInput type="select" name="column#{i}" bind:value={columns[i]}>
+                        <option value={null}>Ignorer</option>
+                        {#each availableColumns.filter(ac => !(columns.filter((_, j) => i !== j).includes(ac))) as ac}
+                            <option value={ac}>{ac}</option>
+                        {/each}
+                    </FormInput>
+                </TableCell>
+            {/each}
 
-        <Table>
-            <svelte:fragment slot="head">
-                <tr>
-                    <th>Article</th>
-                    <th>Quantité disponible</th>
-                    <th>Référence</th>
-                    <th>Fournisseur</th>
-                    <th>Fabricant</th>
-                    <th>Prix</th>
-                </tr>
-            </svelte:fragment>
-        
-            <svelte:fragment slot="body">
-                {#each json as article}
-                    <tr>
-                        <td>{article.name}</td>
-                        <td>{article.quantity}</td>
-                        <td>{article.reference}</td>
-                        <td>{article.supplier}</td>
-                        <td>{(article.internal) ? env.PUBLIC_COMPANY_NAME : article.manufacturer}</td>
-                        <td>{article.price}</td>
-                    </tr>
+            {#each parsed.lines as line}
+                {#each line as item}
+                    <TableCell>{item}</TableCell>
                 {/each}
-            </svelte:fragment>
+            {/each}
         </Table>
     {/await}
 {/if}
-
-<style>
-        
-    th {
-        @apply p-4 border-b border-b-violet-500/75 text-left;
-    }
-
-    td {
-
-        @apply p-4 border-b border-b-violet-500/25;
-    }
-
-    tr:last-child > td{
-        @apply border-0;
-    }
-</style>
