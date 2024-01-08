69749cfc52ce372dac3c62bd20c6826ace70638d
Kworz <romain.sapet@metalizz.com>
2024-01-08 15:24:03 +0100

    added some data validators

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 import { SCMArticleExtension } from "$lib/dataValidation/SCM/SCMArticle";
 import { CRMInterestValidator } from "$lib/dataValidation/CRM/CRMInterest";
+import { SCMAssemblyExtension } from "$lib/dataValidation/SCM/SCMAssembly";
 
 export const prisma = new PrismaClient()
     .$extends(SCMArticleExtension)
+    .$extends(SCMAssemblyExtension)
     .$extends(CRMInterestValidator);
 
 prisma.$connect();
