bfe2ce731cb69a26e3cc0f9aed6b5a4dd6ff6c02
Kworz <romain.sapet@metalizz.com>
2024-01-08 20:47:34 +0100

    camelCase

--- src/routes/app/(scm)/+layout.server.ts
+++ src/routes/app/(scm)/+layout.server.ts
@@ -4,12 +4,12 @@ export const ssr = true;
 
 export const load = (async ({ locals }) => {
 
-    const approx_count = await locals.prisma.scm_order_rows.count({ where: { order: { state: { in: ["acknowledged", "ordered"] }}, received_quantity: { lt: locals.prisma.scm_order_rows.fields.needed_quantity } }});
+    const approxCount = await locals.prisma.scm_order_rows.count({ where: { order: { state: { in: ["acknowledged"] }}, received_quantity: { lt: locals.prisma.scm_order_rows.fields.needed_quantity } }});
 
     return { 
         session: locals.session,
         appSettings: locals.appSettings,
-        approx_count
+        approxCount
     }
     
 }) satisfies LayoutServerLoad;
