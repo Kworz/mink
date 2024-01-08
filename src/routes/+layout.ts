4fa8eb86da36493378b9a05fc6d5110fa4f67e61
Kworz <romain.sapet@metalizz.com>
2024-01-08 02:57:18 +0100

    removed pocketbase

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,15 +1,9 @@
-import { browser } from "$app/environment";
-import { env } from "$env/dynamic/public";
-import { getPocketbase } from "$lib/pocketbase";
 import type { LayoutLoad } from "./$types";
 
 export const load = (async ({ data }) => {
 
-    const pbInstance = browser ? await getPocketbase(data.pbCookie) : await getPocketbase(data.pbCookie, `http://${env.PUBLIC_POCKETBASE_ADDRESS}/`)
-
     return { 
-        user: data.user,
-        pb: pbInstance,
+        user: data.user
     }
 
 }) satisfies LayoutLoad;
