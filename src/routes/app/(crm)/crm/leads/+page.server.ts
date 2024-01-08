481c89af5f9ad2f4db20ba359ef9e481280a9ec6
Kworz <romain.sapet@metalizz.com>
2024-01-08 20:47:25 +0100

    refactored crm leads to use prisma

--- src/routes/app/(crm)/crm/leads/+page.server.ts
+++ src/routes/app/(crm)/crm/leads/+page.server.ts
@@ -1,13 +1,4 @@
-import { Collections, type CrmCompanyResponse, type CrmInterestResponse, type CrmLeadsInterestsResponse, type CrmLeadsResponse } from "$lib/DBTypes";
 import type { PageServerLoad } from "./$types";
-
-export type CrmLeadsExpanded = CrmLeadsResponse<{
-    "crm_leads_interests(lead)": Array<CrmLeadsInterestsResponse<{
-        interest: CrmInterestResponse
-    }>>,
-    "company": CrmCompanyResponse,
-    "company_contacts": Array<CrmCompanyContactResponse>
-}>;
 
 export const load = (async ({ locals, url }) => {
 
@@ -15,10 +6,10 @@ export const load = (async ({ locals, url }) => {
     const filter = url.searchParams.get("filter") ?? "";
     const page = Number(url.searchParams.get("page")) ?? 1;
 
-    const leads = await locals.pb.collection(Collections.CrmLeads).getList<CrmLeadsExpanded>(page, 50, { sort, filter, expand: "crm_leads_interests(lead).interest,company,company_contacts" });
+    const leads = await locals.prisma.crm_leads.findMany({ include: { "interests": true, "company": true, "contacts": true }});
 
     return { 
-        leads: structuredClone(leads)
+        leads
     }
 
 }) satisfies PageServerLoad;
