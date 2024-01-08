a331335c4f1c5f877f224005da6eee6c137d3a58
Kworz <romain.sapet@metalizz.com>
2024-01-08 20:46:59 +0100

    type change for crm company contact

--- src/routes/app/(crm)/crm/companies/CompanyContact.svelte
+++ src/routes/app/(crm)/crm/companies/CompanyContact.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
-    import type { CrmCompanyContactResponse } from "$lib/DBTypes";
+    import type { crm_company_contact } from "@prisma/client";
 
-
-    export let contact: CrmCompanyContactResponse;
+    export let contact: crm_company_contact;
 
 </script>
 
-<a href="/app/crm/companies/{contact.company}" class="py-1 px-3 text-sm text-white bg-zinc-700 ring-1 ring-zinc-500/25 rounded-full inline hover:text-white/75">
+<a href="/app/crm/companies/{contact.company_id}" class="py-1 px-3 text-sm text-white bg-zinc-700 ring-1 ring-zinc-500/25 rounded-full inline hover:text-white/75">
     {contact.name}
 </a>
