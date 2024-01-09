25bb19e33faa8badec44bb76db66dba2fec6eefd
Kworz <romain.sapet@metalizz.com>
2024-01-09 04:03:05 +0100

    updated project page

--- src/routes/app/(scm)/scm/projects/[id]/+page.server.ts
+++ src/routes/app/(scm)/scm/projects/[id]/+page.server.ts
@@ -1,45 +1,21 @@
 import { fail } from "@sveltejs/kit";
 import type { PageServerLoad, Actions } from "./$types";
+import { articleIncludeQuery } from "$lib/components/derived/article/article";
 
 export const load = (async ({ locals, params }) => {
 
     const projectID = params.id;
 
-    const project = await locals.prisma.pr_project.findUniqueOrThrow({ where: { id: projectID } });
+    const project = await locals.prisma.pr_project.findUniqueOrThrow({ where: { id: projectID }, include: {
 
-    /*
+        fabrication_orders: { include: { article: { include: articleIncludeQuery }, askedBy: true, receiver: true }},
+        order_rows: { include: { order: true, article: { include: articleIncludeQuery }}},
+        assembly_buylists: true
 
-    // Fetch all buylist stores and map the article movement to the project
-    const buylistsStores = (await locals.pb.collection(Collections.AssembliesBuylists).getFullList<AssembliesBuylistsResponse>({ filter: `project = "${projectID}"` })).map(buylist => buylist.store);
+    }});
 
-    let filter = `${buylistsStores.map(bls => `store_in = "${bls}"`).join(" || ")}`;
-    filter = (filter.length > 0) ? `(${filter}) && ` : "";
-    filter += `store_out != "" && quantity_update > 0 && article.internal != true`;
-
-    const stores_relations = buylistsStores.length > 0 ? (await locals.pb.collection(Collections.ArticleMovements).getFullList<ArticleMovementsResponse<{ article: ArticleResponse, store_in: StoresResponse, store_out: StoresResponse}>>({ filter, expand: "article,store_in,store_out"})) : [];
-    const articles = await locals.pb.collection(Collections.Article).getFullList<ArticleResponse>({ expand: articleResponseExpand });
-
-    const stores_relationsMapped = stores_relations.map(store_relation => {
-
-        const article = articles.find(a => a.id === store_relation.article)
-
-        if(store_relation.expand && article)
-            store_relation.expand.article = article;
-
-        return store_relation;
-    }) as Array<ArticleMovementsResponse<{ article: ArticleResponseExpanded, store_in: StoresResponse, store_out: StoresResponse}>>;
-
-
-    const order_rows = await locals.pb.collection(Collections.OrdersRows).getFullList<OrderRowsResponseExpanded>({ filter: `project = "${projectID}" && quantity = quantity_received `, expand: 'order,article' });
-    const fabricationOrders = await locals.pb.collection(Collections.FabricationOrders).getFullList<FabricationOrdersResponseExpanded>({ filter: `project = "${projectID}"`, expand: 'article,applicant,receiver'});
-
-    */
-   
     return {
-        project,
-        order_rows: [],
-        fabricationOrders: [],
-        stores_relations: [],
+        project
     }
 
 }) satisfies PageServerLoad;
@@ -49,16 +25,16 @@ export const actions: Actions = {
     editProject: async ({ locals, params, request }) => {
 
         const form = await request.formData();
-
         form.set('closed', String(form.has('closed')));
 
         try
         {
-            //await locals.pb.collection(Collections.Projects).update(params.id, form);
             await locals.prisma.pr_project.update({ where: { id: params.id }, data: { 
-                name: form.get("name"),
-                customer: form.get("customer")
+                name: form.get("name")?.toString(),
+                customer: form.get("customer")?.toString()
             }});
+
+            return { editProject: { success: true }};
         }
         catch(e)
         {
--- src/routes/app/(scm)/scm/projects/[id]/+page.svelte
+++ src/routes/app/(scm)/scm/projects/[id]/+page.svelte
@@ -1,25 +1,22 @@
 <script lang="ts">
     import { browser } from "$app/environment";
     import { invalidateAll } from "$app/navigation";
+    import ArticleRow from "$lib/components/derived/article/ArticleRow.svelte";
+    import User from "$lib/components/derived/user/User.svelte";
     import Button from "$lib/components/generics/Button.svelte";
     import DetailLabel from "$lib/components/generics/DetailLabel.svelte";
+    import Price from "$lib/components/generics/formatters/Price.svelte";
     import FormInput from "$lib/components/generics/inputs/FormInput.svelte";
+    import Flex from "$lib/components/generics/layout/flex.svelte";
+    import MenuSide from "$lib/components/generics/menu/MenuSide.svelte";
     import PillMenu from "$lib/components/generics/pill/pillMenu.svelte";
     import PillMenuButton from "$lib/components/generics/pill/pillMenuButton.svelte";
-    import Wrapper from "$lib/components/generics/containers/Wrapper.svelte";
-    import MenuSide from "$lib/components/generics/menu/MenuSide.svelte";
-    import ArticleRow from "$lib/components/derived/article/ArticleRow.svelte";
-    import Price from "$lib/components/generics/formatters/Price.svelte";
-    import Flex from "$lib/components/generics/layout/flex.svelte";
-    import Store from "$lib/components/derived/store/Store.svelte";
     import Table from "$lib/components/generics/table/Table.svelte";
     import TableCell from "$lib/components/generics/table/TableCell.svelte";
     import TableFootCell from "$lib/components/generics/table/TableFootCell.svelte";
-    import User from "$lib/components/derived/user/User.svelte";
     import { enhanceNoReset } from "$lib/enhanceNoReset";
     import { Temporal } from "@js-temporal/polyfill";
-    import { ArrowRight, Wrench } from "@steeze-ui/heroicons";
-    import { Icon } from "@steeze-ui/svelte-icon";
+    import { Wrench } from "@steeze-ui/heroicons";
     import type { ActionData, PageData } from "./$types";
 
     export let data: PageData;
@@ -59,74 +56,48 @@
 <p>Date de fin: <DetailLabel>{data.project.end_date}</DetailLabel>.</p>
 <p>Affaire cloturée: <DetailLabel>{data.project.closed ? "Oui" : "Non"}</DetailLabel>.</p>
 
-{#if data.fabricationOrders.length > 0}
-    <h3>Ordres de fabrication</h3>
+{#if data.project.fabrication_orders.length > 0}
+    <h2>Ordres de fabrication</h2>
 
     <Table class="self-start" headers={[{ label: "Article demandé" }, { label: "Quantité" }, { label: "Demandeur" }, { label: "Receveur" }, { label: "Date butoire" }]}>
-        {#each data.fabricationOrders as fabOrder}
+        {#each data.project.fabrication_orders as fabricationOrder}
             <TableCell>
-                {#if fabOrder.expand?.article !== undefined}
-                    <ArticleRow article={fabOrder.expand.article} displayStock={false} />
+                {#if fabricationOrder.article !== undefined}
+                    <ArticleRow article={fabricationOrder.article} displayStock={false} />
                 {:else}
                     —
                 {/if}
             </TableCell>
-            <TableCell>{fabOrder.quantity}</TableCell>
+            <TableCell>{fabricationOrder.quantity}</TableCell>
             <TableCell>
-                {#if fabOrder.expand?.applicant !== undefined}
-                    <User user={fabOrder.expand.applicant} />
+                {#if fabricationOrder.askedBy !== undefined}
+                    <User user={fabricationOrder.askedBy} />
                 {/if}
             </TableCell>
             <TableCell>
-                {#if fabOrder.expand?.receiver !== undefined}
-                    <User user={fabOrder.expand.receiver} />
+                {#if fabricationOrder.receiver !== undefined}
+                    <User user={fabricationOrder.receiver} />
                 {/if}
             </TableCell>
-            <TableCell>{fabOrder.end_date}</TableCell>
+            <TableCell>{fabricationOrder.end_date}</TableCell>
         {/each}
     </Table>
 {/if}
 
-{#if data.order_rows.length > 0}
-    <h3>Flux de commandes</h3>
+{#if data.project.order_rows.length > 0}
+    <h2>Flux de commandes</h2>
 
     <Table headers={[{ label: "Article" }, { label: "Commande" }, { label: "Quantité" }, { label: "Prix total" }]}>
-        {#each data.order_rows as order_row}
-            <TableCell><ArticleRow article={order_row.expand?.article} /></TableCell>
-            <TableCell><a href="/app/scm/orders/{order_row.order}">{order_row.expand?.order.name}</a></TableCell>
-            <TableCell>{order_row.quantity}</TableCell>
-            <TableCell><Price value={order_row.quantity * (order_row.ack_price ?? 0)} /></TableCell>
+        {#each data.project.order_rows as order_row}
+            <TableCell><ArticleRow article={order_row.article} /></TableCell>
+            <TableCell><a href="/app/scm/orders/{order_row.order_id}">{order_row.order.name}</a></TableCell>
+            <TableCell>{order_row.needed_quantity}</TableCell>
+            <TableCell><Price value={order_row.needed_quantity * (order_row.ack_price ?? 0)} /></TableCell>
         {/each}
 
         <TableFootCell colspan={3}>Total flux de commandes</TableFootCell>
         <TableFootCell>
-            <Price value={data.order_rows.reduce((acc, row) => acc + (row.quantity * (row.ack_price ?? 0)), 0)} />
+            <Price value={data.project.order_rows.reduce((acc, row) => acc + (row.needed_quantity * (row.ack_price ?? 0)), 0)} />
         </TableFootCell>
     </Table>
-{/if}
-
-{#if data.stores_relations.length > 0}
-    <Wrapper class="mt-6">
-        <h3>Sorties de stock</h3>
-    
-        <Table headers={[{ label: "Article" }, { label: "Quantité" }, { label: "Movement" }, { label: "Prix total" }]}>
-            {#each data.stores_relations as store_relation}
-                <TableCell><ArticleRow article={store_relation.expand?.article} displayManufacturer /></TableCell>
-                <TableCell>{store_relation.quantity_update}</TableCell>
-                <TableCell>
-                    <Flex items="center">
-                        <Store store={store_relation.expand?.store_out}/>
-                        <Icon src={ArrowRight} class="h-4 w-4 text-violet-500" />
-                        <Store store={store_relation.expand?.store_in} />
-                    </Flex>
-                </TableCell>
-                <TableCell><Price value={(((store_relation.expand?.article.expand?.["article_price(article)"]?.at(0)?.price) ?? store_relation.expand?.article.price) ?? 0) * store_relation.quantity_update} /></TableCell>
-            {/each}
-
-            <TableFootCell colspan={3}>Total flux sortie de stock</TableFootCell>
-            <TableFootCell>
-                <Price value={data.stores_relations.reduce((acc, sr) => acc + (Math.abs(sr.quantity_update) * (((sr.expand?.article.expand?.["article_price(article)"]?.at(0)?.price) ?? sr.expand?.article.price) ?? 0)), 0)} />
-            </TableFootCell>
-        </Table>
-    </Wrapper>
 {/if}
