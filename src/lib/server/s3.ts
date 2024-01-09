2eac2ebb2b1fa2fc8ffd8b5865cfef1ba20bc420
Kworz <romain.sapet@metalizz.com>
2024-01-09 22:12:30 +0100

    added s3

---
+++ src/lib/server/s3.ts
@@ -0,0 +1,9 @@
+import { S3Client } from "@aws-sdk/client-s3";
+
+export const s3Client = new S3Client({
+    region: process.env.AWS_REGION as string,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string
+    }
+});
