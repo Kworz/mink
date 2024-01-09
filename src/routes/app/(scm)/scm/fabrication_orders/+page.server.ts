79d5d80c05763bf67123291059533f1ec8dd02a1
Kworz <romain.sapet@metalizz.com>
2024-01-09 02:36:26 +0100

    updated include query

--- src/routes/app/(scm)/scm/fabrication_orders/+page.server.ts
+++ src/routes/app/(scm)/scm/fabrication_orders/+page.server.ts
@@ -1,8 +1,9 @@
+import { articleIncludeQuery } from "$lib/components/derived/article/article";
 import type { PageServerLoad } from "./$types";
 
 export const load = (async ({ locals }) => {
 
-    const fabricationOrders = await locals.prisma.scm_fabrication_order.findMany();
+    const fabricationOrders = await locals.prisma.scm_fabrication_order.findMany({ include: { article: { include: articleIncludeQuery }, project: true, askedBy: true, receiver: true }});
 
     return {
         fabricationOrders
