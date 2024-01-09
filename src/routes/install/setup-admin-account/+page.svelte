a4600e3cdd3f97e63b4a7fd1b5c605410642b98e
Kworz <romain.sapet@metalizz.com>
2024-01-09 23:32:58 +0100

    added autocomplete on admin account setup

--- src/routes/install/setup-admin-account/+page.svelte
+++ src/routes/install/setup-admin-account/+page.svelte
@@ -2,11 +2,9 @@
     import { enhance } from "$app/forms";
     import Button from "$lib/components/generics/Button.svelte";
     import FormInput from "$lib/components/generics/inputs/FormInput.svelte";
-
     import type { ActionData } from "./$types";
 
     export let form: ActionData;
-
 </script>
 
 <h4>Création du compte administrateur</h4>
@@ -15,10 +13,10 @@
 
 <form action="?/createFirstAccount" method="post" use:enhance class="flex flex-col gap-6 mt-6">
 
-    <FormInput label="Nom d'utilisateur" name="username" type="text" required labelMandatory />
-    <FormInput label="Adresse email" name="email" type="email" required labelMandatory />
-    <FormInput label="Mot de passe" name="password" type="password" required labelMandatory />
-    <FormInput label="Mot de passe (confirmation)" name="password_confirmation" type="password" required labelMandatory />
+    <FormInput label="Nom d'utilisateur" name="username" type="text" required labelMandatory autocomplete="username" />
+    <FormInput label="Adresse email" name="email" type="email" required labelMandatory autocomplete="email" />
+    <FormInput label="Mot de passe" name="password" type="password" required labelMandatory autocomplete="new-password" />
+    <FormInput label="Mot de passe (confirmation)" name="password_confirmation" type="password" required labelMandatory autocomplete="new-password" />
 
     <Button class="w-full">Créer le compte</Button>
 
