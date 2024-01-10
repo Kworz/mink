67e9eaeedd4651002069cd0ca1c1efea8bcc7a84
Kworz <romain.sapet@metalizz.com>
2024-01-10 22:53:28 +0100

    removed onDestroy hook, always shows the close button and title

--- src/lib/components/generics/menu/MenuSide.svelte
+++ src/lib/components/generics/menu/MenuSide.svelte
@@ -4,26 +4,20 @@
     import { Icon } from "@steeze-ui/svelte-icon";
     import Portal from "svelte-portal";
 
-    import { createEventDispatcher, onDestroy, onMount } from "svelte";
+    import { createEventDispatcher, onMount } from "svelte";
 
-    const dispatch = createEventDispatcher();
+    const dispatch = createEventDispatcher<{ close: void }>();
 
-    export let closable = false;
-    export let noBlur = false;
-    export let title: string | undefined = undefined;
+    export let title: string;
 
     onMount(() => {
-        if(noBlur) return; 
-
         document.getElementById("main_content")!.style.filter = "blur(4px)";
         document.getElementById("navbar")!.style.filter = "blur(4px)";
-    });
 
-    onDestroy(() => {
-        if(noBlur) return;
-
-        document.getElementById("main_content")!.style.filter = "";
-        document.getElementById("navbar")!.style.filter = "";
+        return () => {
+            document.getElementById("main_content")!.style.filter = "";
+            document.getElementById("navbar")!.style.filter = "";
+        }
     });
 
 </script>
@@ -31,17 +25,13 @@
 <svelte:window on:keydown={e => e.key === "Escape" && dispatch("close")} />
 
 <Portal target="body">
-    <div class="absolute z-50 top-4 right-4 bottom-4 rounded-xl shrink-0 ring-1 ring-zinc-400/25 bg-zinc-800 shadow-2xl duration-300 p-4 w-1/4 overflow-y-scroll">
+    <div class="absolute z-50 top-4 right-4 bottom-4 rounded-xl shrink-0 ring-1 ring-zinc-400/25 bg-zinc-800 shadow-2xl duration-300 p-4 min-w-[25%] overflow-y-scroll">
+        <button class="absolute top-4 right-4" on:click={() => dispatch("close")}>
+            <Icon src={XMark} class="h-4 w-4 text-red-500" />
+        </button>
 
-        {#if closable}
-            <button class="absolute top-4 right-4" on:click={() => dispatch("close")}>
-                <Icon src={XMark} class="h-4 w-4 text-red-500" />
-            </button>
-        {/if}
-        {#if title}
-            <h3 class="mr-12">{title}</h3>
-            <div class="mt-3 mb-4 h-[1px] w-full bg-zinc-400/25" />
-        {/if}
+        <h3 class="mr-12">{title}</h3>
+        <div class="mt-3 mb-4 h-[1px] w-full bg-zinc-400/25" />
         
         <div class="w-full duration-300 px-[1px] overflow-y-scroll">
             <slot />
