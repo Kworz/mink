3a95e50641f148c1bad9b3331913b9c39e6fd3b7
Kworz <romain.sapet@metalizz.com>
2024-01-10 16:39:10 +0100

    extended prisma to return the aws ressource directly for supplier logo

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,5 +1,15 @@
+import { env } from "$env/dynamic/private";
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient().$extends({
+    result: {
+        scm_supplier: {
+            logo: {
+                needs: { id: true, logo: true },
+                compute({ id, logo }) { return (logo === null) ? null : `https://${env.AWS_BUCKET_NAME}.s3.${env.AWS_REGION}.amazonaws.com/scm/supplier/${id}/${logo}` }
+            }
+        }
+    }
+});
 
 prisma.$connect();
