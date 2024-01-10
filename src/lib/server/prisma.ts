8d82c903910c76f5ab127863cbac06023c65289c
Kworz <romain.sapet@metalizz.com>
2024-01-10 22:54:03 +0100

    removed prisma extension, non-pojo return is so blocking

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,15 +1,5 @@
-import { env } from "$env/dynamic/private";
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient().$extends({
-    result: {
-        scm_supplier: {
-            logo: {
-                needs: { id: true, logo: true },
-                compute({ id, logo }) { return (logo === null) ? null : `https://${env.AWS_BUCKET_NAME}.s3.${env.AWS_REGION}.amazonaws.com/scm/supplier/${id}/${logo}` }
-            }
-        }
-    }
-});
+export const prisma = new PrismaClient();
 
 prisma.$connect();
