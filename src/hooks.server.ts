98e52797a988bbc08031f0af49176f3b8fa2b247
Kworz <romain.sapet@metalizz.com>
2024-01-15 21:07:19 +0100

    good redirects for setup now

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,14 +3,13 @@ import type { PrismaClient } from '@prisma/client';
 import { auth } from "$lib/server/lucia";
 import { prisma } from "$lib/server/prisma";
 import { getSettings } from '$lib/server/settings';
-import { s3Client } from '$lib/server/s3';
+import { getS3Client } from '$lib/server/s3';
 import { locale } from 'svelte-i18n';
 
 export const handle = (async ({ event, resolve }) => {
 
     // locals hydration
     event.locals.prisma = prisma as unknown as PrismaClient; // ? extended Prisma client looses its type information needs this to be casted
-    event.locals.s3 = s3Client;
     event.locals.lucia = auth(event.locals.prisma).handleRequest(event);
     event.locals.session = await event.locals.lucia.validate();
 
@@ -24,10 +23,15 @@ export const handle = (async ({ event, resolve }) => {
         return new Response(null, {status: 303, headers: { 'location': `/install` }});
     else if(appSettings !== undefined && event.route.id?.startsWith("/install"))
         return new Response(null, {status: 303, headers: { 'location': `/app` }});
+    else if(appSettings === undefined && event.route.id?.startsWith("/install"))
+        return await resolve(event);
     else if(appSettings === undefined)
-        throw "App settings are undefined.";
+        throw new Error("App settings are undefined");
 
     event.locals.appSettings = appSettings;
+
+    // set S3 after gettings the settings
+    event.locals.s3 = getS3Client(appSettings.app_s3_region);
     
     if(event.route.id?.startsWith("/app") && event.locals.session === null)
     {
