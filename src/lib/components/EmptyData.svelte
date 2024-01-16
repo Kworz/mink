233b85822884ba5bf07c173c6d32ac9175f8dd76
Kworz <romain.sapet@metalizz.com>
2024-01-16 03:14:40 +0100

    emptydata i18n

--- src/lib/components/EmptyData.svelte
+++ src/lib/components/EmptyData.svelte
@@ -3,15 +3,16 @@
     import Wrapper from "./generics/containers/Wrapper.svelte";
     import { Icon } from "@steeze-ui/svelte-icon";
     import { CubeTransparent } from "@steeze-ui/heroicons";
+    import { _ } from "svelte-i18n";
 </script>
 
 <Wrapper class="mt-6">
     <div class="flex flex-row gap-8 items-center">
         <Icon src={CubeTransparent} class="w-36 text-violet-500" theme="solid" />
         <div>
-            <h2>Aucun élément à afficher 😅</h2>
-            <p class="mb-4">Vous n'avez pas encore créé d'élément ici!</p>
-            <Button on:click size="small" class="self-center">Créer votre premier élément</Button>
+            <h2>{$_('app.empty_data.title')}</h2>
+            <p class="mb-4">{$_('app.empty_data.description')}</p>
+            <Button on:click size="small" class="self-center">{$_('app.empty_data.action')}</Button>
         </div>
     </div>
 </Wrapper>
