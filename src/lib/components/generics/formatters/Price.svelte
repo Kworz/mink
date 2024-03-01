ecfb79953ee155846c3d55ab3037caa6738344c7
Kworz <romain.sapet@metalizz.com>
2024-03-01 13:55:50 +0100

    prevent NaN

--- src/lib/components/generics/formatters/Price.svelte
+++ src/lib/components/generics/formatters/Price.svelte
@@ -2,7 +2,7 @@
     export let value: number | null | undefined;
 </script>
 
-{#if value === 0 || value === undefined || value === null}
+{#if value === 0 || value === undefined || value === null || isNaN(value)}
     — €
 {:else}
     {new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(value)}
