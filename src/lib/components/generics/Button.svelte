72209f8848168040575b4711737669e175e49f35
Kworz <romain.sapet@metalizz.com>
2024-03-01 09:13:16 +0100

    added confirm icon for button

--- src/lib/components/generics/Button.svelte
+++ src/lib/components/generics/Button.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
-    import { ArrowPath } from "@steeze-ui/heroicons";
+    import { ArrowPath, ExclamationTriangle } from "@steeze-ui/heroicons";
     import { Icon } from "@steeze-ui/svelte-icon";
 
     export let size: "base" | "small" | "tiny" = "base";
     export let role: "primary" | "secondary" | "tertiary" | "danger" | "warning" | "success" = "primary";
 
+    
     export let form: string | undefined = undefined;
     export let preventSend = false;
-
+    
+    export let confirm = false; // To be used only when role="danger2"
     export let suspense = false;
     
     const sizes: Record<typeof size, string> = {
@@ -22,7 +24,7 @@
         "tertiary": "hover:bg-gray-500 border-gray-500",
 
         "warning": "hover:bg-amber-500 border-amber-500",
-        "danger": "hover:bg-red-500 border-red-500",
+        "danger": (!confirm) ? "hover:bg-red-500 border-red-500" : "hover:bg-red-600 border-red-600",
         "success": "hover:bg-emerald-500 border-emerald-500"
     };
 
@@ -30,6 +32,7 @@
 
 <button {form} type={preventSend ? "button" : "submit"} class="flex flex-row gap-3 items-center text-white duration-100 rounded-md {sizes[size]} {roles[role]} {$$props.class}" on:click>
     {#if suspense} <Icon src={ArrowPath} class="animate-spin w-5 h-5 inline" /> {/if}
+    {#if confirm && role === "danger"} <Icon src={ExclamationTriangle} class="w-5 h-5 inline" /> {/if}
     <div>
         <slot />
     </div>
