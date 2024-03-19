5e5132b08623aa6f6386a377e6b40295c2db86ab
Kworz <romain.sapet@metalizz.com>
2024-03-19 08:41:32 +0100

    table checkbox

---
+++ src/lib/components/generics/table/TableCellCheckbox.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import TableCell from "./TableCell.svelte";
+
+    export let group: Array<string>;
+    export let value: string;
+</script>
+
+<TableCell class="items-center"><input type="checkbox" bind:group {value} /></TableCell>
