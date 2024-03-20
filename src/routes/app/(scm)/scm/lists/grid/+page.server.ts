9c5dbafb424b78cc755afbd4606a9a687079e025
Kworz <romain.sapet@metalizz.com>
2024-03-20 14:22:11 +0100

    updated grid to work with flatten assembly, work must be done still

--- src/routes/app/(scm)/scm/lists/grid/+page.server.ts
+++ src/routes/app/(scm)/scm/lists/grid/+page.server.ts
@@ -1,3 +1,5 @@
+import { flattenAssembly } from '$lib/components/derived/assemblies/flattenAssembly';
+import { articleIncludeQuery } from '$lib/components/derived/article/article';
 import type { PageServerLoad } from "./$types";
 
 import { redirect } from "@sveltejs/kit";
@@ -6,21 +8,43 @@ export const load = (async ({ locals, url }) => {
 
     const ids = url.searchParams.get("ids");
 
-    if(ids === null) return redirect(303, "/app/scm/lists?noIdGiven=true");
+    if(ids === null)
+        return redirect(303, "/app/scm/lists?gridNoIds=true");
 
-    const lists = await locals.prisma.scm_assembly_buylist.findMany({ where: { id: { in: ids.split(",") }}, include: { assembly: true, project: true }});
-    const referenceList = lists.at(0);
+    const listsAssemblies = await locals.prisma.scm_assembly_buylist.findMany({ where: { id: { in: ids.split(",") }}, include: { assembly: true }});
 
-    // Check if all lists are from the same assembly
-    if(referenceList === undefined || lists.some(list => list.assembly !== referenceList.assembly)) return redirect(303, "/app/scm/lists");
+    const referenceList = listsAssemblies.at(0);
+
+    if(referenceList === undefined || listsAssemblies.length === 0 || listsAssemblies.some(list => list.assembly.id !== referenceList.assembly.id))
+        return redirect(303, "/app/scm/lists?gridDifferentAssemblies=true");
+
+    const lists = [];
+
+    for(const id of ids.split(","))
+    {
+        const list = await locals.prisma.scm_assembly_buylist.findUnique({ where: { id }, include: { assembly: true, project: true }});
+        if(list === null) return redirect(303, "/app/scm/lists?listNotFound=true");
+            
+        const listStoreRelations = await locals.prisma.scm_store_relation.findMany({ where: { store_id: list.store_id }, include: { article: { include: articleIncludeQuery }}});
+        const flattenedAssembly = await flattenAssembly(list.assembly_id, locals.prisma);
+
+        lists.push({
+            list,
+            listStoreRelations,
     
-    const flattenAssemblyReference = await flatAssembly(referenceList.assembly_id, locals.prisma); //TODO: Replace old assembly flattening function
-    const storeRelations = await locals.prisma.scm_store_relation.findMany({ where: { store_id: { in: lists.map(k => k.store_id) }}});
+            flattenedAssembly
+        });
+    }
 
+    const articleFilter = url.searchParams.get("articleFilter") ? JSON.parse(decodeURIComponent(url.searchParams.get("articleFilter") as string)) : undefined;
+    const articleSort = url.searchParams.get("articleSort") ? JSON.parse(decodeURIComponent(url.searchParams.get("articleSort") as string)) : undefined;
+
+    const articles = await locals.prisma.scm_article.findMany({ where: {...{ id: { in: Object.keys(lists.at(0)!.flattenedAssembly) }}, ...articleFilter }, include: articleIncludeQuery, orderBy: articleSort });
+    
     return {
         lists,
-        storeRelations,
-        flattenAssemblyReference: structuredClone(flattenAssemblyReference),
-    };
+
+        articles
+    }
 
 }) satisfies PageServerLoad;
--- src/routes/app/(scm)/scm/lists/grid/+page.svelte
+++ src/routes/app/(scm)/scm/lists/grid/+page.svelte
@@ -1,39 +1,42 @@
 <script lang="ts">
-    import { invalidateAll } from "$app/navigation";
+    import { goto, invalidateAll } from "$app/navigation";
+    import { _ } from "svelte-i18n";
     import ArticleRow from "$lib/components/derived/article/ArticleRow.svelte";
     import DetailLabel from "$lib/components/generics/DetailLabel.svelte";
-    import type { FilterCondition } from "$lib/components/derived/filter/filter";
-    import Filter2 from "$lib/components/derived/filter/Filter.svelte";
+    import PillMenu from "$lib/components/generics/pill/pillMenu.svelte";
+    import PillMenuButton from "$lib/components/generics/pill/pillMenuButton.svelte";
+    import Filter from "$lib/components/derived/filter/Filter.svelte";
     import FormInput from "$lib/components/generics/inputs/FormInput.svelte";
     import Table from "$lib/components/generics/table/Table.svelte";
     import TableCell from "$lib/components/generics/table/TableCell.svelte";
-    import Wrapper from "$lib/components/generics/containers/Wrapper.svelte";
-    import { enhanceNoReset } from "$lib/enhanceNoReset";
+    import TableCellCheckbox from "$lib/components/generics/table/TableCellCheckbox.svelte";
+    import { QrCode } from "@steeze-ui/heroicons";
 
-    import type { ActionData, PageData, Snapshot } from "./$types";
+    import type { PageData } from "./$types";
+    import type { ActionData } from "../[id]/$types";
     import Flex from "$lib/components/generics/layout/flex.svelte";
     import Button from "$lib/components/generics/Button.svelte";
     import RoundedLabel from "$lib/components/generics/RoundedLabel.svelte";
     import TableFootCell from "$lib/components/generics/table/TableFootCell.svelte";
     import Price from "$lib/components/generics/formatters/Price.svelte";
     import { computeArticlePrice } from "$lib/components/derived/article/article";
+    import { page } from "$app/stores";
+    import { browser } from "$app/environment";
+    import { enhanceApply } from "$lib/enhanceApply";
     
     export let data: PageData;
     export let form: ActionData;
 
-    let filters: Array<FilterCondition> = [];
-    let filter: string = "";
+    let filter = $page.url.searchParams.has("articleFilter") ? JSON.parse(decodeURIComponent($page.url.searchParams.get("articleFilter")!)) : {};
+    let sort = $page.url.searchParams.has("articleSort") ? JSON.parse(decodeURIComponent($page.url.searchParams.get("articleSort")!)) : {};
 
     let selected: string[] = [];
 
-    $: if(form !== null && form?.buyListRelationEdit?.success) { filter = ""; invalidateAll(); setTimeout(() => { form = null; }, 2500) };
+    const refresh = () => { if(browser) { goto(`?ids=${data.lists.map(ld => ld.list.id).join(",")}&articleFilter=${encodeURIComponent(JSON.stringify(filter))}&articleSort=${encodeURIComponent(JSON.stringify(sort))}`) } }
 
-    export const snapshot: Snapshot<FilterCondition[]> = {
-        capture: () => filters,
-        restore: (value) => { filters = value; }
-    }
+    $: filter, sort, refresh();
 
-    $: allSelected = selected.length === data.flattenAssemblyReference.length;
+    $: if(form?.buyListRelationEdit?.success) { invalidateAll(); setTimeout(() => { form = null; }, 3000); };
 
 </script>
 
@@ -41,113 +44,124 @@
     <title>Grille de listes</title>
 </svelte:head>
 
-<Wrapper>
-    <h3 class="mb-3">Grille de listes</h3>
-    <p>Nombre de listes: <DetailLabel>{data.lists.length}</DetailLabel>.</p>
-</Wrapper>
+<h1>Grille de listes</h1>
+<p>Nombre de listes: <DetailLabel>{data.lists.length}</DetailLabel>.</p>
 
-<Wrapper class="mt-6">
-
+<PillMenu message={selected.length > 0 ? $_('scm.articles.selected', { values: { n: selected.length }}) : undefined}>
     {#if selected.length > 0}
-        <a href="/app/scm/articles/print?images=true&articles={selected.join(",")}" class="mb-6"><Button size="small">Imprimer les etiquettes</Button></a>
+        <PillMenuButton icon={QrCode} click={() => window.open(`/app/scm/articles/print?images=true&lists=${selected.join(",")}`, '_blank')?.focus()}>Imprimer les etiquettes</PillMenuButton>
     {/if}
+</PillMenu>
 
-    <Filter2 bind:filter bind:filters availableFilters={[{name: "article.name", default: true, type: "string" }, { name: "quantity", type: "number" }, { name: "manufacturer", type: "string" }, { name: "reference", type: "string" }, { name: "supplier.name", type: "array" }, { name: "valid", type: "boolean" }, { name: "stock", type: "boolean" }]} />
-    
-    <Table 
-        headers={["selectAll", { label: "Article"}, { label: "Total" }, ...data.lists.map(k => { return { label: k.name }})]}
-        class="mt-6"
-        on:selectall={() => { selected = (allSelected) ? [] : data.flattenAssemblyReference.map(far => far.article_child_id)}}
-        bind:allSelected
-    >
-        {#each data.flattenAssemblyReference as assemblyRow}
+<Filter class="my-6" bind:filter availableFilters={[
+    { name: "name", default: true, type: "string" },
+    { name: "reference", type: "string" },
+    { name: "brand", type: "string" },
+    { name: "critical_quantity", type: "number" },
+    { name: "consumable", type: "boolean" }]}
+/>
 
-            <TableCell>
-                <input type="checkbox" bind:group={selected} value={assemblyRow.article_child_id} />
-            </TableCell>
+<Table 
+    headers={["selectAll", { label: `${$_('app.generic.article')} (${data.articles.length})` }, { label: $_('app.generic.total') }, ...data.lists.map(k => { return { label: k.list.name }})]}
+    bind:selected
+    selectables={data.articles.map(a => a.id)}
+>
+    {#each data.articles as article}
+        <TableCellCheckbox bind:group={selected} value={article.id} />
 
-            <TableCell>
-                <ArticleRow article={assemblyRow.article_child} displayStock displayInboundSupplies />
-            </TableCell>
+        <TableCell><ArticleRow article={article} displayStock displayInboundSupplies /></TableCell>
 
-            <TableCell>
-                {@const itemsAdded = data.storeRelations.filter(r => r.article_id === assemblyRow.article_child_id).reduce((p, c) => p + c.quantity, 0)}
-                {@const itemsTotal = assemblyRow.quantity * data.lists.length}
-                <RoundedLabel role={(itemsAdded != itemsTotal ? (itemsAdded === 0) ? "danger" : "warning" : "success")} class="self-center">{`${itemsAdded} / ${itemsTotal}`}</RoundedLabel>
-            </TableCell>
+        <TableCell>
+            {@const itemsAdded = data.lists.flatMap(l => l.listStoreRelations).filter(r => r.article_id === article.id).reduce((p, c) => p + c.quantity, 0)}
+            {@const articleRequiredQuantity = data.lists[0].flattenedAssembly[article.id].requiredQuantity}
+            {@const itemsTotal = articleRequiredQuantity * data.lists.length}
+            <RoundedLabel role={(itemsAdded != itemsTotal ? (itemsAdded === 0) ? "danger" : "warning" : "success")} class="self-center">{`${itemsAdded} / ${itemsTotal}`}</RoundedLabel>
+        </TableCell>
 
-            {#each data.lists as list}
-                {@const buyListRelation = data.storeRelations.find((r) => r.store_id === list.store_id && r.article_id === assemblyRow.article_child_id)}
+        {#each data.lists as listData}
 
-                <TableCell position="center">
-                    <form action="/app/scm/lists/grid/?/buyListRelationEdit" method="post" use:enhanceNoReset class="flex gap-4 items-center">
+            {@const formData = form?.buyListRelationEdit?.article === article.id && form?.buyListRelationEdit?.list === listData.list.id ? form.buyListRelationEdit : undefined}
 
-                        {#if form?.buyListRelationEdit?.[list.id]?.[assemblyRow.article_child_id]}
-                            {@const data = form?.buyListRelationEdit?.[list.id]?.[assemblyRow.article_child_id]}
+            {@const associatedStoreRelation = listData.listStoreRelations.find(lsr => lsr.article_id === article.id)}
 
-                            {#if data.storesToGetFrom !== undefined}
-                                <FormInput type="select" name="store" label="Choisir un stock de provenance" required>
-                                    {#each data.storesToGetFrom as store}
-                                        <option value={store.id}>{store.name}</option>
-                                    {/each}
-                                </FormInput>
-                            {/if}
+            {@const storeQuantity = associatedStoreRelation?.quantity ?? 0}
+            {@const requiredQuantity = listData.flattenedAssembly[article.id].requiredQuantity}
 
-                            {#if data.storesToSendTo !== undefined}
-                                <FormInput type="select" name="store" label="Choisir un stock de destination">
-                                    {#each data.storesToSendTo as store}
-                                        <option value={store.id}>{store.name}</option>
-                                    {/each}
-                                </FormInput>
-                            {/if}
+            {@const requiredPrice = (requiredQuantity - storeQuantity) * computeArticlePrice(article.order_rows)}
+            {@const totalPrice = requiredQuantity * computeArticlePrice(article.order_rows)}
+            {@const isValid = requiredQuantity <= storeQuantity}
+
+            <TableCell position="center">
+                <form action="/app/scm/lists/{listData.list.id}?/buyListRelationEdit" method="post" use:enhanceApply class="flex gap-4 items-center">
+
+                    {#if formData}
+                        {#if formData.storesToGetFrom !== undefined}
+                            <FormInput type="select" name="store" label="Choisir un stock de provenance" required>
+                                {#each formData.storesToGetFrom as store}
+                                    <option value={store.id}>{store.name}</option>
+                                {/each}
+                            </FormInput>
                         {/if}
 
-                        <input type="hidden" name="article" value={assemblyRow.article_child_id} />
-                        <input type="hidden" name="list" value={list.id} />
+                        {#if formData.storesToSendTo !== undefined}
+                            <FormInput type="select" name="store" label="Choisir un stock de destination">
+                                {#each formData.storesToSendTo as store}
+                                    <option value={store.id}>{store.name}</option>
+                                {/each}
+                            </FormInput>
+                        {/if}
+                    {/if}
 
-                        <Flex items="center" gap={2}>
-                            <FormInput 
-                                name="quantity"
-                                type="number" 
-                                step={assemblyRow.article_child.unit === "" ? 1 : 0.1} 
-                                value={buyListRelation?.quantity ?? 0}
-                                min={0}
-                                max={assemblyRow.quantity}
-                                valid={buyListRelation?.quantity >= assemblyRow.quantity}
-                                invalid={form?.buyListRelationEdit?.[list.id]?.[assemblyRow.article_child_id]?.error !== undefined}
-                                label={form?.buyListRelationEdit?.[list.id]?.[assemblyRow.article_child_id]?.error ?? (form?.buyListRelationEdit?.[list.id]?.[assemblyRow.article_child_id]?.success ?? undefined)}
-                                validateOnChange
-                            />
-                            /
-                            <span class="font-medium">{assemblyRow.quantity}</span>
-                        </Flex>
-                    </form>
-                </TableCell>
-            {/each}
+                    <input type="hidden" name="article" value={article.id} />
+                    <input type="hidden" name="list" value={listData.list.id} />
+
+                    <Flex items="center" gap={2}>
+                        <FormInput 
+                            name="quantity"
+                            type="number"
+                            step={article.unit_quantity === null ? undefined : article.unit_quantity} 
+                            value={associatedStoreRelation?.quantity ?? 0}
+                            min={0}
+                            max={requiredQuantity}
+                            valid={associatedStoreRelation?.quantity === requiredQuantity}
+                            invalid={formData?.error !== undefined}
+                            label={formData?.error ?? formData?.success ?? undefined}
+                            validateOnChange
+                        />
+                        /
+                        <span class="font-medium">{requiredQuantity}</span>
+                    </Flex>
+                </form>
+            </TableCell>
         {/each}
+    {/each}
 
-        <TableFootCell />
-        <TableFootCell>Total (Restant / Total)</TableFootCell>
+<!--
+<TableFootCell />
+    <TableFootCell>Total (Restant / Total)</TableFootCell>
 
+    <TableFootCell>
+
+        {@const articles = data.articles.}
+
+        <Price value={data.lists.reduce((pl, cl) => pl + data.flattenAssemblyReference.reduce((p, c) => p + (computeArticlePrice(c.article_child.order_rows) * (c.quantity - (data.storeRelations.find(r => r.article_id === c.article_child_id && r.store_id == cl.store_id)?.quantity ?? 0))), 0), 0)}/>
+            /
+        <Price value={Object.keys(data.lists[0].flattenedAssembly).reduce((p, c) => p + (computeArticlePrice(data.articles.find(a => a.id === c)?.order_rows ?? [])) * data.lists[0].flattenedAssembly[c].requiredQuantity, 0) * data.lists.length} />
+
+    </TableFootCell>
+
+    {#each data.lists as list}
         <TableFootCell>
 
-            <Price value={data.lists.reduce((pl, cl) => pl + data.flattenAssemblyReference.reduce((p, c) => p + (computeArticlePrice(c.article_child.order_rows) * (c.quantity - (data.storeRelations.find(r => r.article_id === c.article_child_id && r.store_id == cl.store_id)?.quantity ?? 0))), 0), 0)}/>
-                /
-            <Price value={data.flattenAssemblyReference.reduce((p, c) => p + (computeArticlePrice(c.article_child.order_rows)) * c.quantity, 0) * data.lists.length} />
+            {@const buyListRelations = data.storeRelations.filter((r) => r.store_id === list.store_id)}
+
+            <Price value={data.flattenAssemblyReference.reduce((p, c) => p + (computeArticlePrice(c.article_child.order_rows) * (c.quantity - (buyListRelations.find(r => r.article_id === c.article_child_id)?.quantity ?? 0))), 0)}/>
+            /
+            <Price value={data.flattenAssemblyReference.reduce((p, c) => p + computeArticlePrice(c.article_child.order_rows) * c.quantity, 0)} />
 
         </TableFootCell>
+    {/each}
+-->
+    
 
-        {#each data.lists as list}
-            <TableFootCell>
-
-                {@const buyListRelations = data.storeRelations.filter((r) => r.store_id === list.store_id)}
-
-                <Price value={data.flattenAssemblyReference.reduce((p, c) => p + (computeArticlePrice(c.article_child.order_rows) * (c.quantity - (buyListRelations.find(r => r.article_id === c.article_child_id)?.quantity ?? 0))), 0)}/>
-                /
-                <Price value={data.flattenAssemblyReference.reduce((p, c) => p + computeArticlePrice(c.article_child.order_rows) * c.quantity, 0)} />
-
-            </TableFootCell>
-        {/each}
-
-    </Table>
-</Wrapper>
+</Table>
