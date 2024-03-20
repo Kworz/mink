7ef666068bf7df639d2de45df816522d6101cc02
Kworz <romain.sapet@metalizz.com>
2024-03-20 23:00:10 +0100

    added emptyDataFilter component, to display when filters are too restrictives

---
+++ src/lib/components/EmptyDataFilter.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Wrapper from "./generics/containers/Wrapper.svelte";
+    import { Icon } from "@steeze-ui/svelte-icon";
+    import { DocumentMagnifyingGlass } from "@steeze-ui/heroicons";
+    import { _ } from "svelte-i18n";
+</script>
+
+<Wrapper>
+    <div class="flex flex-row gap-8 items-center">
+        <Icon src={DocumentMagnifyingGlass} class="w-36 text-violet-500" theme="solid" />
+        <div>
+            <h2>{$_('app.empty-data-filter.title')}</h2>
+            <p class="mb-4">{$_('app.empty-data-filter.description')}</p>
+        </div>
+    </div>
+</Wrapper>
