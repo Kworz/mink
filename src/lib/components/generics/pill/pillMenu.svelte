25b92eb4f7430f675456c427ba3b291396bbbe94
Kworz <romain.sapet@metalizz.com>
2024-04-04 12:38:10 +0200

    pillmenu has a dedicated section for selected elements

--- src/lib/components/generics/pill/pillMenu.svelte
+++ src/lib/components/generics/pill/pillMenu.svelte
@@ -7,6 +7,7 @@
     const { open } = setPillMenuContext();
 
     export let message: string | undefined = undefined;
+    export let selection: number | undefined = undefined;
 </script>
 
 <div class="absolute top-10 right-10 flex flex-col items-end gap-4">
@@ -20,6 +21,13 @@
     {#if $open}
         <div class="bg-zinc-700 ring-1 ring-zinc-400/50 z-50 rounded-md p-4 flex flex-col items-start gap-3 w-max" in:scale={{ duration: 100 }} out:scale={{ duration:100 }} use:clickOutside on:click_outside={() => { if($open === true) { $open = false } }}>
             <slot />
+            {#if selection !== undefined && selection > 0}
+                <div class="flex gap-2 w-full items-center">
+                    <span class="text-sm">Sélection</span>
+                    <div class="grow bg-zinc-400/50 w-auto h-[1px]" />
+                </div>
+                <slot name="selection" />
+            {/if}
         </div>
     {/if}
 </div>
