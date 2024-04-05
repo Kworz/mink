637145fad3796c90119e03cc7e1f45b6948d1e02
Kworz <romain.sapet@metalizz.com>
2024-04-05 09:23:02 +0200

    removed buttons for table page, replaced by a select

--- src/lib/components/generics/table/TablePages.svelte
+++ src/lib/components/generics/table/TablePages.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-    import Button from "$lib/components/generics/Button.svelte";
     import Flex from "$lib/components/generics/layout/flex.svelte";
     import FormInput from "../inputs/FormInput.svelte";
 
@@ -10,21 +9,14 @@
 </script>
 
 {#if totalPages > 1}
-    <Flex class="mt-6 {$$props.class}" items="center">
-        {#each [...Array(totalPages).keys()] as number}
-            {@const roleMinus = (currentPage <= 0) ? number + 1 === 1 : number + 1 === currentPage}
-            {@const rolePlus = (currentPage > totalPages) ? number + 1 === totalPages : number + 1 === currentPage}
-            <Button 
-                class="aspect-square w-8"
-                size="tiny"
-                role={(roleMinus || rolePlus) ? "primary" : "tertiary"}
-                on:click={() => currentPage = (number+1) }
-            >{number + 1}</Button>
-        {/each}
+    <Flex class="{$$props.class}" items="center">
+        <FormInput type="select" name="page" bind:value={currentPage} label="Page">
+            {#each [...Array(totalPages).keys()] as number}
+                <option value={number + 1}>{number + 1}</option>
+            {/each}
+        </FormInput>
 
-        <span class="ml-2">Éléments par page</span>
-
-        <FormInput type="select" name="page-items-count" bind:value={itemsPerPage}>
+        <FormInput type="select" name="page-items-count" bind:value={itemsPerPage} label="Éléments par page">
             <option value={50}>50</option>
             <option value={100}>100</option>
             <option value={200}>200</option>
