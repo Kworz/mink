6c93007cb65fcd52a6ec86a49371bf2c9c5cec52
Kworz <romain.sapet@metalizz.com>
2024-05-01 10:45:24 +0200

    added error pages

---
+++ src/routes/error/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+
+    let data: { error: string, message: string } | null = null;
+
+    $: data = $page.url.searchParams.has("error") ? JSON.parse(decodeURIComponent($page.url.searchParams.get("error")!)) : null;
+</script>
+
+<main class="p-8">
+    <h1 class="text-red-500">{data?.error || "Erreur"}</h1>
+    <p>{data?.message || "Internal error"}</p>
+</main>
---
+++ src/routes/install/config-error/+page.svelte
@@ -0,0 +1,2 @@
+<h1>There is a configuration error</h1>
+<p>Please update the environment variables.</p>
