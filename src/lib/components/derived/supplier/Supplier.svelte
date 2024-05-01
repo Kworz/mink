d7b8a3fff39e511c3a6d5934fc984d7cca7b30bc
Kworz <romain.sapet@metalizz.com>
2024-05-01 10:39:24 +0200

    use s3 api to get files

--- src/lib/components/derived/supplier/Supplier.svelte
+++ src/lib/components/derived/supplier/Supplier.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
     import { browser } from "$app/environment";
-    import { page } from "$app/stores";
     import type { scm_supplier } from "@prisma/client";
 
+    export let supplier: scm_supplier;
+    
     $: margin = supplier.logo !== null  ? "pl-1 pr-3" : "px-3";
 
-    export let supplier: scm_supplier;
 </script>
 
 {#if browser}
-    <a href={`/app/scm/articles?filter=${encodeURIComponent('supplier ~ "' + supplier.id + '"')}`} class="flex flex-row gap-2 items-center bg-zinc-900 hover:bg-zinc-900/80 duration-100 {margin} py-1 rounded-full ring-1 ring-zinc-400/25 w-max">
+    <span class="flex flex-row gap-2 items-center bg-zinc-900 hover:bg-zinc-900/80 duration-100 {margin} py-1 rounded-full ring-1 ring-zinc-400/25 w-max">
         {#if supplier.logo !== null}
-            <img src="https://{$page.data.appSettings.app_s3_bucketname}.s3.{$page.data.appSettings.app_s3_region}.amazonaws.com/scm/supplier/{supplier.id}/{supplier.logo}" alt={supplier.name + " Logo"} class="h-6 w-6 rounded-full" />
+            <img src="/api/file/scm/supplier/{supplier.id}/{supplier.logo}" alt={supplier.name + " Logo"} class="h-6 w-6 rounded-full" />
         {/if}
         <span class="text-sm font-medium text-nowrap">{supplier.name}</span>
-    </a>
+    </span>
 {/if}
