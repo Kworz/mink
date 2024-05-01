4c3cdf6696c87427416af8f05b755cd791123b64
Kworz <romain.sapet@metalizz.com>
2024-05-01 10:38:17 +0200

    S3 uses endpoint rather than region and bucketname

--- src/lib/server/s3.ts
+++ src/lib/server/s3.ts
@@ -1,9 +1,11 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
-export const getS3Client = (region: string) => new S3Client({
-    region,
+export const getS3Client = () => new S3Client({
+    endpoint: process.env.S3_ENDPOINT as string,
+    region: "us-east-1",
+    forcePathStyle: true,
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string
+        accessKeyId: process.env.S3_ACCESS_KEY_ID as string,
+        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY as string
     }
 });
