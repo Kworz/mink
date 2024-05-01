68f64cdbdf7cf74409c54cc89d955067c1c83891
Kworz <romain.sapet@metalizz.com>
2024-05-01 10:40:22 +0200

    created s3 api file browser

---
+++ src/routes/api/file/[...filePath]/+server.ts
@@ -0,0 +1,27 @@
+import { GetObjectCommand } from "@aws-sdk/client-s3";
+import type { RequestHandler } from "./$types";
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export const GET: RequestHandler = async ({ locals, params }) => {
+
+    if(locals.session === null || locals.user === null)
+        return new Response("Not authorized", { status: 401 });
+
+    try
+    {
+        const result = await locals.s3.send(new GetObjectCommand({
+            Bucket: process.env.S3_BUCKET_NAME as string,
+            Key: params.filePath
+        }));
+
+        return new Response(await result.Body?.transformToByteArray(), { status: 200, headers: {
+            'Content-Type': String(result.ContentType),
+            'Content-Length': String(result.ContentLength)
+        }});
+    }
+    catch(ex)
+    {
+        console.error(ex);
+        return new Response("Not found", { status: 404 });
+    }
+}
