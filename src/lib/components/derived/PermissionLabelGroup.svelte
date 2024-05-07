03e3e61395e6001f1886559e31eea859a43d802d
Kworz <romain.sapet@metalizz.com>
2024-05-07 21:58:59 +0200

    permission label group

---
+++ src/lib/components/derived/PermissionLabelGroup.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import type { permission } from "$lib/prisma-enums";
+    import { _ } from "svelte-i18n";
+    import RoundedLabel, { type Roles } from "../generics/RoundedLabel.svelte";
+    import Flex from "../generics/layout/flex.svelte";
+
+    export let permission: permission | null;
+
+    const permissionColors: Record<"r" | "c" | "u" | "d", Roles> = {
+        "r": "primary",
+        "u": "warning",
+        "c": "success",
+        "d": "danger",
+    };
+
+    $: permissionSplitted = (permission?.split("") ?? []) as ("r" | "c" | "u" | "d")[];
+
+</script>
+
+<Flex gap={2}>
+    {#if permission === null}
+        <RoundedLabel role="secondary" size="sm">{$_('app.permission.null')}</RoundedLabel>
+    {:else}
+        {#each permissionSplitted as item}
+            <RoundedLabel role={permissionColors[item]} size="sm">{$_(`app.permission.solo_values.${item}`)}</RoundedLabel>
+        {/each}
+    {/if}
+</Flex>
+
