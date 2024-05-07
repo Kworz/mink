93170ae9a2cbac525167fe789a0acc8a229b4574
Kworz <romain.sapet@metalizz.com>
2024-05-07 21:58:53 +0200

    boolean label

---
+++ src/lib/components/derived/BooleanLabel.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { _ } from "svelte-i18n";
+    import RoundedLabel from "../generics/RoundedLabel.svelte";
+    import type { ComponentProps } from "svelte";
+
+    export let value: boolean;
+
+    // Additional properties
+    export let format: "truefalse" | "yesno" = "yesno";
+    export let size: ComponentProps<RoundedLabel>["size"] = "sm";
+</script>
+
+<RoundedLabel {size} role={value ? "success" : "danger"}>
+    {$_(`app.generic.${format === "yesno" ? "boolean-other" : "boolean"}.${value}`)}
+</RoundedLabel>
